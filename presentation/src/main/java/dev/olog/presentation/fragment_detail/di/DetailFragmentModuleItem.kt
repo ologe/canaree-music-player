8c28ebd1dfe74540f079fe615825ae72c3eb7a13
Eugeniu Olog <eugeniu.olog@gmail.com>
2017-12-06 20:05:58 +0100

    commit

--- presentation/src/main/java/dev/olog/presentation/fragment_detail/di/DetailFragmentModuleItem.kt
+++ presentation/src/main/java/dev/olog/presentation/fragment_detail/di/DetailFragmentModuleItem.kt
@@ -1,5 +1,6 @@
 package dev.olog.presentation.fragment_detail.di
 
+import android.content.res.Resources
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoMap
@@ -16,23 +17,34 @@ class DetailFragmentModuleItem {
     @Provides
     @IntoMap
     @StringKey(MediaIdHelper.MEDIA_ID_BY_FOLDER)
-    internal fun provideFolderItem(mediaId: String, useCase: GetFolderUseCase) : Flowable<DisplayableItem> {
+    internal fun provideFolderItem(
+            resources: Resources,
+            mediaId: String,
+            useCase: GetFolderUseCase) : Flowable<DisplayableItem> {
+
         return useCase.execute(mediaId)
-                .map { it.toHeaderItem() }
+                .map { it.toHeaderItem(resources) }
     }
 
     @Provides
     @IntoMap
     @StringKey(MediaIdHelper.MEDIA_ID_BY_PLAYLIST)
-    internal fun providePlaylistItem(mediaId: String, useCase: GetPlaylistUseCase) : Flowable<DisplayableItem> {
+    internal fun providePlaylistItem(
+            resources: Resources,
+            mediaId: String,
+            useCase: GetPlaylistUseCase) : Flowable<DisplayableItem> {
+
         return useCase.execute(mediaId)
-                .map { it.toHeaderItem() }
+                .map { it.toHeaderItem(resources) }
     }
 
     @Provides
     @IntoMap
     @StringKey(MediaIdHelper.MEDIA_ID_BY_ALBUM)
-    internal fun provideAlbumItem(mediaId: String, useCase: GetAlbumUseCase) : Flowable<DisplayableItem> {
+    internal fun provideAlbumItem(
+            mediaId: String,
+            useCase: GetAlbumUseCase) : Flowable<DisplayableItem> {
+
         return useCase.execute(mediaId)
                 .map { it.toHeaderItem() }
     }
@@ -40,17 +52,25 @@ class DetailFragmentModuleItem {
     @Provides
     @IntoMap
     @StringKey(MediaIdHelper.MEDIA_ID_BY_ARTIST)
-    internal fun provideArtistItem(mediaId: String, useCase: GetArtistUseCase) : Flowable<DisplayableItem> {
+    internal fun provideArtistItem(
+            resources: Resources,
+            mediaId: String,
+            useCase: GetArtistUseCase) : Flowable<DisplayableItem> {
+
         return useCase.execute(mediaId)
-                .map { it.toHeaderItem() }
+                .map { it.toHeaderItem(resources) }
     }
 
     @Provides
     @IntoMap
     @StringKey(MediaIdHelper.MEDIA_ID_BY_GENRE)
-    internal fun provideGenreItem(mediaId: String, useCase: GetGenreUseCase) : Flowable<DisplayableItem> {
+    internal fun provideGenreItem(
+            resources: Resources,
+            mediaId: String,
+            useCase: GetGenreUseCase) : Flowable<DisplayableItem> {
+
         return useCase.execute(mediaId)
-                .map { it.toHeaderItem() }
+                .map { it.toHeaderItem(resources) }
     }
 
 
--- presentation/src/main/java/dev/olog/presentation/fragment_detail/model/DetailItemMapper.kt
+++ presentation/src/main/java/dev/olog/presentation/fragment_detail/model/DetailItemMapper.kt
@@ -1,27 +1,29 @@
 package dev.olog.presentation.fragment_detail.model
 
+import android.content.res.Resources
 import dev.olog.domain.entity.*
 import dev.olog.presentation.R
 import dev.olog.presentation.model.DisplayableItem
+import dev.olog.shared.TextUtils
 
 private const val HEADER_ID = "header media id"
 
-fun Folder.toHeaderItem(): DisplayableItem {
+fun Folder.toHeaderItem(resources: Resources): DisplayableItem {
     return DisplayableItem(
             R.layout.item_detail_info_image,
             HEADER_ID,
             title.capitalize(),
-            null,
+            resources.getQuantityString(R.plurals.song_count, this.size, this.size).toLowerCase(),
             image
     )
 }
 
-fun Playlist.toHeaderItem(): DisplayableItem {
+fun Playlist.toHeaderItem(resources: Resources): DisplayableItem {
     return DisplayableItem(
             R.layout.item_detail_info_image,
             HEADER_ID,
             title.capitalize(),
-            null,
+            resources.getQuantityString(R.plurals.song_count, this.size, this.size).toLowerCase(),
             image
     )
 }
@@ -36,22 +38,27 @@ fun Album.toHeaderItem(): DisplayableItem {
     )
 }
 
-fun Artist.toHeaderItem(): DisplayableItem {
+fun Artist.toHeaderItem(resources: Resources): DisplayableItem {
+    val songs = resources.getQuantityString(R.plurals.song_count, this.songs, this.songs)
+    val albums = if (this.albums == 0) "" else {
+        "${resources.getQuantityString(R.plurals.album_count, this.albums, this.albums)}${TextUtils.MIDDLE_DOT_SPACED}"
+    }
+
     return DisplayableItem(
             R.layout.item_detail_info_image,
             HEADER_ID,
             name,
-            null,
+            "$albums$songs".toLowerCase(),
             image
     )
 }
 
-fun Genre.toHeaderItem(): DisplayableItem {
+fun Genre.toHeaderItem(resources: Resources): DisplayableItem {
     return DisplayableItem(
             R.layout.item_detail_info_image,
             HEADER_ID,
             name,
-            null,
+            resources.getQuantityString(R.plurals.song_count, this.size, this.size).toLowerCase(),
             image
     )
 }
