ce22e0b32f22a929ad15c393536693700f7935fd
Eugeniu Olog <eugeniu.olog@gmail.com>
2017-12-20 22:43:10 +0100

    adjustin playlist images

--- presentation/src/main/java/dev/olog/presentation/activity_about/AboutActivity.kt
+++ presentation/src/main/java/dev/olog/presentation/activity_about/AboutActivity.kt
@@ -1,7 +1,15 @@
 package dev.olog.presentation.activity_about
 
+import android.content.res.ColorStateList
+import android.graphics.Typeface
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.ViewSwitcher
 import dev.olog.presentation.R
 import dev.olog.presentation._base.BaseActivity
 import kotlinx.android.synthetic.main.activity_about.*
@@ -21,7 +29,10 @@ class AboutActivity : BaseActivity() {
         list.adapter = adapter
         list.setHasFixedSize(true)
 
-        header.setCurrentText(getString(R.string.about))
+        switcher.setFactory(factory)
+        switcher.setCurrentText(getString(R.string.about))
+        switcher.setInAnimation(this, R.anim.slide_up_fade_in)
+        switcher.setOutAnimation(this, R.anim.slide_up_fade_out)
     }
 
     override fun onResume() {
@@ -33,5 +44,18 @@ class AboutActivity : BaseActivity() {
     override fun onPause() {
         super.onPause()
         back.setOnClickListener(null)
+    }
+
+    private val factory = ViewSwitcher.ViewFactory {
+        val textView = TextView(this@AboutActivity)
+        textView.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        textView.gravity = Gravity.CENTER_HORIZONTAL
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+        textView.typeface = Typeface.DEFAULT_BOLD
+        textView.setTextColor(ColorStateList.valueOf(
+                ContextCompat.getColor(this@AboutActivity, R.color.text_color_primary)))
+        textView
     }
 }
