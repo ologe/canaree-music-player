bf1ef1b4187daf04e17651568493f4073a25b3ec
Eugeniu Olog <eugeniu.olog@gmail.com>
2017-12-30 19:47:50 +0100

    merged classes interfaces

--- presentation/src/main/java/dev/olog/presentation/activity_preferences/PreferencesActivity.kt
+++ presentation/src/main/java/dev/olog/presentation/activity_preferences/PreferencesActivity.kt
@@ -3,12 +3,23 @@ package dev.olog.presentation.activity_preferences
 import android.os.Bundle
 import android.preference.PreferenceActivity
 import dev.olog.presentation.R
+import kotlinx.android.synthetic.main.activity_preferences.*
 
 class PreferencesActivity : PreferenceActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preferences)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        back.setOnClickListener { onBackPressed() }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        back.setOnClickListener(null)
     }
 
 }
