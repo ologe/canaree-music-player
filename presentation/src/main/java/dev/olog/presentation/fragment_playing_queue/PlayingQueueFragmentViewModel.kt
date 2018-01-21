0132c85ebf4b81c0ae3cfb9afb5745fd00742ee0
Eugeniu Olog <eugeniu.olog@gmail.com>
2018-01-21 23:15:37 +0100

    fixes

--- presentation/src/main/java/dev/olog/presentation/fragment_playing_queue/PlayingQueueFragmentViewModel.kt
+++ presentation/src/main/java/dev/olog/presentation/fragment_playing_queue/PlayingQueueFragmentViewModel.kt
@@ -11,6 +11,8 @@ import dev.olog.shared.MediaId
 import dev.olog.shared.groupMap
 import dev.olog.shared_android.TextUtils
 import dev.olog.shared_android.extension.asLiveData
+import io.reactivex.Flowable
+import java.util.concurrent.TimeUnit
 
 class PlayingQueueFragmentViewModel(
         observePlayingQueueUseCase: ObservePlayingQueueUseCase,
@@ -23,7 +25,11 @@ class PlayingQueueFragmentViewModel(
         var idInPlaylist = -1
     }
 
-    val data = observePlayingQueueUseCase.execute()
+    val data = Flowable.merge(
+            observePlayingQueueUseCase.execute().take(1),
+            observePlayingQueueUseCase.execute().skip(1)
+                    .debounce(500, TimeUnit.MILLISECONDS))
+            .distinctUntilChanged()
             .groupMap { it.toPlayingQueueDisplayableItem() }
             .asLiveData()
 
