8edcd15719f7ddadebf74de1708754f039ae4939
Eugeniu Olog <eugeniu.olog@gmail.com>
2018-02-18 18:45:48 +0100

    cleaning

--- app/src/main/java/dev/olog/msc/presentation/library/tab/TabOnAppBarScrollListener.kt
+++ app/src/main/java/dev/olog/msc/presentation/library/tab/TabOnAppBarScrollListener.kt
@@ -2,7 +2,9 @@ package dev.olog.msc.presentation.library.tab
 
 import android.support.design.widget.AppBarLayout
 import android.widget.FrameLayout
+import dev.olog.msc.R
 import kotlinx.android.synthetic.main.fragment_tab.view.*
+import org.jetbrains.anko.dimen
 import org.jetbrains.anko.dip
 import javax.inject.Inject
 
@@ -11,8 +13,8 @@ class TabOnAppBarScrollListener @Inject constructor(
 
 ) : AppBarLayout.OnOffsetChangedListener {
 
-    private val marginBottom = fragment.context!!.dip(96)
-    private val marginTop = fragment.context!!.dip(32)
+    private val marginBottom = fragment.context!!.dimen(R.dimen.tab_fast_scroller_margin_bottom)
+    private val marginTop = fragment.context!!.dip(R.dimen.tab_fast_scroller_margin_top)
 
     override fun onOffsetChanged(appBarLayout: AppBarLayout?, verticalOffset: Int) {
         val view = fragment.view
