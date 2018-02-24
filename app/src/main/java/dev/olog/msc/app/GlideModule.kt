ccbe52806335b3c4ce75c5a482be15d31a2d4b45
Eugeniu Olog <eugeniu.olog@gmail.com>
2018-02-24 17:16:38 +0100

    cleaning

--- app/src/main/java/dev/olog/msc/app/GlideModule.kt
+++ app/src/main/java/dev/olog/msc/app/GlideModule.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.load.engine.executor.GlideExecutor
+import com.bumptech.glide.load.engine.executor.GlideExecutor.UncaughtThrowableStrategy.IGNORE
 import com.bumptech.glide.module.AppGlideModule
 import com.bumptech.glide.request.RequestOptions
 
@@ -14,17 +16,21 @@ class GlideModule : AppGlideModule() {
 
     override fun applyOptions(context: Context, builder: GlideBuilder) {
 
+        builder.setLogLevel(Log.ERROR)
+                .setDefaultRequestOptions(defaultRequestOptions(context))
+                .setDiskCacheExecutor(GlideExecutor.newDiskCacheExecutor(IGNORE))
+                .setSourceExecutor(GlideExecutor.newSourceExecutor(IGNORE))
+                .build(context)
+    }
+
+    private fun defaultRequestOptions(context: Context): RequestOptions {
         val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
 
-        val defaultOptions = RequestOptions()
+        return RequestOptions()
                 // Prefer higher quality images unless we're on a low RAM device
                 .format(if (activityManager.isLowRamDevice)
                     DecodeFormat.PREFER_RGB_565 else DecodeFormat.PREFER_ARGB_8888
                 ).disallowHardwareConfig()
-
-        builder.setLogLevel(Log.ERROR)
-                .setDefaultRequestOptions(defaultOptions)
-                .build(context)
     }
 
     override fun isManifestParsingEnabled(): Boolean = false
