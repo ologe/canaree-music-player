fad272b376aa2a09bb73a0bf4bec284f511359a4
Eugeniu Olog <eugeniu.olog@gmail.com>
2018-02-26 17:09:33 +0100

    cleaning

--- app/src/main/java/dev/olog/msc/presentation/edit/song/EditSongFragment.kt
+++ app/src/main/java/dev/olog/msc/presentation/edit/song/EditSongFragment.kt
@@ -7,6 +7,7 @@ import android.arch.lifecycle.Observer
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.support.annotation.StringRes
 import com.bumptech.glide.Priority
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.jakewharton.rxbinding2.widget.RxTextView
@@ -28,6 +29,7 @@ class EditSongFragment : BaseFragment() {
         const val TAG = "EditInfoFragment"
         const val ARGUMENTS_MEDIA_ID = "$TAG.arguments.media_id"
 
+        @JvmStatic
         fun newInstance(mediaId: MediaId): EditSongFragment {
             return EditSongFragment().withArguments(
                     ARGUMENTS_MEDIA_ID to mediaId.toString())
@@ -44,7 +46,7 @@ class EditSongFragment : BaseFragment() {
                 .map { it.view().text.toString() }
                 .map { it.isNotBlank() }
                 .asLiveData()
-                .subscribe(this, { okButton.isEnabled = it })
+                .subscribe(this,okButton::setEnabled)
 
         viewModel.observeData()
                 .observe(this, Observer {
@@ -57,7 +59,7 @@ class EditSongFragment : BaseFragment() {
                         disc.setText(it.disc)
                         trackNumber.setText(it.track)
                     } else {
-                        context!!.toast("info not found")
+                        ctx.toast(R.string.edit_song_info_not_found)
                     }
                     hideLoader()
                 })
@@ -67,14 +69,14 @@ class EditSongFragment : BaseFragment() {
                     if (it != null){
                         setImage(it)
                     } else {
-                        context!!.toast("image not found")
+                        ctx.toast(R.string.edit_song_image_not_found)
                     }
                     hideLoader()
                 })
 
         viewModel.observeConnectivity()
                 .asLiveData()
-                .subscribe(this, { context!!.toast(it) })
+                .subscribe(this, { ctx.toast(it) })
     }
 
     override fun onResume() {
@@ -85,9 +87,7 @@ class EditSongFragment : BaseFragment() {
                             disc.extractText(), trackNumber.extractText())
 
             when (result){
-                UpdateResult.OK -> {
-                    activity!!.onBackPressed()
-                }
+                UpdateResult.OK -> act.onBackPressed()
                 UpdateResult.EMPTY_TITLE -> context!!.toast("title can not be null")
                 UpdateResult.ILLEGAL_DISC_NUMBER -> context!!.toast("invalid disc number")
                 UpdateResult.ILLEGAL_TRACK_NUMBER -> context!!.toast("invalid track number")
@@ -95,10 +95,10 @@ class EditSongFragment : BaseFragment() {
                 UpdateResult.ERROR -> context!!.toast(R.string.popup_error_message)
             }
         }
-        cancelButton.setOnClickListener { activity!!.onBackPressed() }
+        cancelButton.setOnClickListener { act.onBackPressed() }
         autoTag.setOnClickListener {
             viewModel.fetchSongInfo()
-            showLoader("Fetching song info")
+            showLoader(R.string.edit_song_fetching_info)
         }
         changeAlbumArt.setOnClickListener {
 
@@ -113,12 +113,12 @@ class EditSongFragment : BaseFragment() {
                         when (which){
                             0 -> {
                                 viewModel.fetchAlbumArt()
-                                showLoader("Fetching image")
+                                showLoader(R.string.edit_song_fetching_image)
                             }
                             1 -> {
                                 val intent = Intent(Intent.ACTION_PICK)
                                 intent.type = "image/*"
-                                act.startActivityForResult(intent, RESULT_LOAD_IMAGE)
+                                startActivityForResult(intent, RESULT_LOAD_IMAGE)
                             }
                             2 -> {
                                 viewModel.restoreAlbumArt()
@@ -137,15 +137,10 @@ class EditSongFragment : BaseFragment() {
         changeAlbumArt.setOnClickListener(null)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
         super.onActivityResult(requestCode, resultCode, data)
-        println(data)
-        if (resultCode == Activity.RESULT_OK){
-            if (requestCode == RESULT_LOAD_IMAGE){
-                data?.data?.let {
-                    viewModel.setAlbumArt(it)
-                }
-            }
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == Activity.RESULT_OK){
+            data.data?.let { viewModel.setAlbumArt(it) }
         }
     }
 
@@ -165,6 +160,10 @@ class EditSongFragment : BaseFragment() {
 
         builder.into(cover)
         builder.into(backgroundCover)
+    }
+
+    private fun showLoader(@StringRes resId: Int){
+        showLoader(getString(resId))
     }
 
     private fun showLoader(message: String){
