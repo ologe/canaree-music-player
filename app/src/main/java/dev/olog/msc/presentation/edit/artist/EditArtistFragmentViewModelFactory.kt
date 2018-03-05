f339c6e24b5be881d964129e2e95e8b1ffc7ec3b
Eugeniu Olog <eugeniu.olog@gmail.com>
2018-03-05 04:31:01 +0100

    cleaning

--- app/src/main/java/dev/olog/msc/presentation/edit/artist/EditArtistFragmentViewModelFactory.kt
+++ app/src/main/java/dev/olog/msc/presentation/edit/artist/EditArtistFragmentViewModelFactory.kt
@@ -3,16 +3,19 @@ package dev.olog.msc.presentation.edit.artist
 import android.app.Application
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
+import javax.inject.Inject
 
-class EditArtistFragmentViewModelFactory(
-        private val application: Application
+class EditArtistFragmentViewModelFactory @Inject constructor(
+        private val application: Application,
+        private val presenter: EditArtistFragmentPresenter
 
 ) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return EditArtistFragmentViewModel(
-                application
+                application,
+                presenter
         ) as T
     }
 }
