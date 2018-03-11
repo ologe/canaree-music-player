c21d1075a2762b420084950f951baab9dadf806b
Eugeniu Olog <eugeniu.olog@gmail.com>
2018-03-11 15:01:40 +0100

    cleaning

--- app/src/main/java/dev/olog/msc/app/App.kt
+++ app/src/main/java/dev/olog/msc/app/App.kt
@@ -1,7 +1,6 @@
 package dev.olog.msc.app
 
 import android.app.AlarmManager
-import android.arch.lifecycle.ProcessLifecycleOwner
 import android.content.Context
 import android.preference.PreferenceManager
 import dagger.android.AndroidInjector
@@ -30,8 +29,6 @@ class App : DaggerApplication() {
         initializeDebug()
         initializeConstants()
         resetSleepTimer()
-
-        ProcessLifecycleOwner.get().lifecycle
 
 //        validation.isValid().subscribe({}, Throwable::printStackTrace)
     }
