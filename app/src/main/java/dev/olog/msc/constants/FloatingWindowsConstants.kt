894a9ba1863948c0a10085f9d5c94fb79bdc87d6
Eugeniu Olog <eugeniu.olog@gmail.com>
2018-04-24 18:35:10 +0200

    1.0.17 - cleaning

--- app/src/main/java/dev/olog/msc/constants/FloatingWindowsConstants.kt
+++ app/src/main/java/dev/olog/msc/constants/FloatingWindowsConstants.kt
@@ -3,6 +3,6 @@ package dev.olog.msc.constants
 object FloatingWindowsConstants {
 
     private const val TAG = "FloatingWindowsConstants"
-    const val ACTION_START_SERVICE = TAG + ".action.start"
+    const val ACTION_START_SERVICE = "$TAG.action.start"
 
 }
