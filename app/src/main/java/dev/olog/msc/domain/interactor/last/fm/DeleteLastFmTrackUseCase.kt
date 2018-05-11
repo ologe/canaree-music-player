c91dd7fa6543c8e7688323a823f715198ac59df8
Eugeniu Olog <eugeniu.olog@gmail.com>
2018-05-11 08:59:48 +0000

    Update DeleteLastFmTrackUseCase.kt

--- app/src/main/java/dev/olog/msc/domain/interactor/last/fm/DeleteLastFmTrackUseCase.kt
+++ app/src/main/java/dev/olog/msc/domain/interactor/last/fm/DeleteLastFmTrackUseCase.kt
@@ -6,25 +6,26 @@ import dev.olog.msc.domain.interactor.base.CompletableUseCaseWithParam
 import io.reactivex.Completable
 import javax.inject.Inject
 
-class DeleteLastFmTrackUseCase @Inject constructor(
-        private val gateway: LastFmGateway
+//class DeleteLastFmTrackUseCase @Inject constructor(
+//        private val gateway: LastFmGateway
+//
+//) : WorkerUseCaseWithParam() {
+//
+//    companion object {
+//        private const val TAG = "DeleteLastFmTrackUseCase"
+//        const val TRACK_ID = "$TAG.track"
+//    }
+//
+//    override fun buildUseCase(input: Data): Worker.WorkerResult {
+//        val id = input.getLong(TRACK_ID, -1)
+//
+//        gateway.deleteTrack(id)
+//
+//        return Worker.WorkerResult.SUCCESS
+//    }
+//
+//}
 
-) : WorkerUseCaseWithParam() {
-
-    companion object {
-        private const val TAG = "DeleteLastFmTrackUseCase"
-        const val TRACK_ID = "$TAG.track"
-    }
-
-    override fun buildUseCase(input: Data): Worker.WorkerResult {
-        val id = input.getLong(TRACK_ID, -1)
-
-        gateway.deleteTrack(id)
-
-        return Worker.WorkerResult.SUCCESS
-    }
-
-}
 
 
 class DeleteLastFmTrackUseCase @Inject constructor(
