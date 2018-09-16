3319614a77824b4408c100800a5e55fba287da89
Eugeniu Olog <eugeniu.olog@gmail.com>
2018-09-16 18:34:26 +0200

    1.2.5 - tried leak fix

--- app/src/main/java/dev/olog/msc/presentation/utils/images/RippleTarget.kt
+++ app/src/main/java/dev/olog/msc/presentation/utils/images/RippleTarget.kt
@@ -1,5 +1,6 @@
 package dev.olog.msc.presentation.utils.images
 
+import android.annotation.SuppressLint
 import android.graphics.drawable.Drawable
 import android.support.v4.content.ContextCompat
 import android.support.v7.graphics.Palette
@@ -12,12 +13,15 @@ import dev.olog.msc.presentation.widget.parallax.ParallaxImageView
 import dev.olog.msc.utils.RippleUtils
 import dev.olog.msc.utils.isMarshmallow
 import dev.olog.msc.utils.k.extension.getBitmap
+import java.lang.ref.WeakReference
 
 class RippleTarget(
-        private val imageView: ImageView,
+        view: ImageView,
         private val isLeaf : Boolean
 
-) : DrawableImageViewTarget(imageView), Palette.PaletteAsyncListener {
+) : DrawableImageViewTarget(view), Palette.PaletteAsyncListener {
+
+    private val imageView = WeakReference(view)
 
     init {
         if (isLeaf && imageView is ForegroundImageView){
@@ -28,23 +32,26 @@ class RippleTarget(
     override fun onResourceReady(drawable: Drawable, transition: Transition<in Drawable>?) {
         super.onResourceReady(drawable, transition)
 
-        if (!isLeaf && imageView is ForegroundImageView){
+        if (!isLeaf && imageView.get() is ForegroundImageView){
             val bitmap = drawable.getBitmap() ?: return
             Palette.from(bitmap).clearFilters().generate(this)
         }
     }
 
+    @SuppressLint("NewApi")
     override fun onGenerated(palette: Palette?) {
-        if (!isLeaf && imageView is ForegroundImageView){
+        val view = imageView.get() ?: return
+
+        if (!isLeaf && view is ForegroundImageView){
             val fallbackColor = ContextCompat.getColor(view.context, R.color.mid_grey)
             val darkAlpha = if (isMarshmallow()) .4f else .1f
             val lightAlpha = if (isMarshmallow()) .5f else .2f
 
-            imageView.foreground = RippleUtils.create(palette, darkAlpha,
+            view.foreground = RippleUtils.create(palette, darkAlpha,
                     lightAlpha, fallbackColor, true)
 
-            if (imageView is ParallaxImageView){
-                imageView.setScrimColor(RippleUtils.createColor(palette, darkAlpha,
+            if (view is ParallaxImageView){
+                view.setScrimColor(RippleUtils.createColor(palette, darkAlpha,
                         lightAlpha, fallbackColor))
             }
         }
