09f020fdf9ffa2a12e3a2a25c870c7d6b3212bed
Eugeniu Olog <eugeniu.olog@energee3.com>
2018-09-25 00:25:03 +0200

    1.2.5 - grouped insert and delete recent searches

---
+++ app/src/main/java/dev/olog/msc/domain/interactor/search/delete/DeleteRecentSearchUseCase.kt
@@ -0,0 +1,29 @@
+package dev.olog.msc.domain.interactor.search.delete
+
+import dev.olog.msc.domain.executors.IoScheduler
+import dev.olog.msc.domain.gateway.RecentSearchesGateway
+import dev.olog.msc.domain.interactor.base.CompletableUseCaseWithParam
+import dev.olog.msc.utils.MediaId
+import io.reactivex.Completable
+import javax.inject.Inject
+
+class DeleteRecentSearchUseCase @Inject constructor(
+        scheduler: IoScheduler,
+        private val recentSearchesGateway: RecentSearchesGateway
+
+) : CompletableUseCaseWithParam<MediaId>(scheduler) {
+
+    @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
+    override fun buildUseCaseObservable(mediaId: MediaId): Completable {
+        val id = mediaId.resolveId
+        return when {
+            mediaId.isLeaf -> recentSearchesGateway.deleteSong(id)
+            mediaId.isArtist -> recentSearchesGateway.deleteArtist(id)
+            mediaId.isAlbum -> recentSearchesGateway.deleteAlbum(id)
+            mediaId.isPlaylist -> recentSearchesGateway.deletePlaylist(id)
+            mediaId.isFolder -> recentSearchesGateway.deleteFolder(id)
+            mediaId.isGenre -> recentSearchesGateway.deleteGenre(id)
+            else -> throw IllegalArgumentException("invalid category ${mediaId.resolveId}")
+        }
+    }
+}
---
+++ app/src/main/java/dev/olog/msc/domain/interactor/search/insert/InsertRecentSearchUseCase.kt
@@ -0,0 +1,30 @@
+package dev.olog.msc.domain.interactor.search.insert
+
+import dev.olog.msc.domain.executors.IoScheduler
+import dev.olog.msc.domain.gateway.RecentSearchesGateway
+import dev.olog.msc.domain.interactor.base.CompletableUseCaseWithParam
+import dev.olog.msc.utils.MediaId
+import io.reactivex.Completable
+import javax.inject.Inject
+
+
+class InsertRecentSearchUseCase @Inject constructor(
+        scheduler: IoScheduler,
+        private val recentSearchesGateway: RecentSearchesGateway
+
+) : CompletableUseCaseWithParam<MediaId>(scheduler) {
+
+    @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
+    override fun buildUseCaseObservable(mediaId: MediaId): Completable {
+        val id = mediaId.resolveId
+        return when {
+            mediaId.isLeaf -> recentSearchesGateway.insertSong(id)
+            mediaId.isArtist -> recentSearchesGateway.insertArtist(id)
+            mediaId.isAlbum -> recentSearchesGateway.insertAlbum(id)
+            mediaId.isPlaylist -> recentSearchesGateway.insertPlaylist(id)
+            mediaId.isFolder -> recentSearchesGateway.insertFolder(id)
+            mediaId.isGenre -> recentSearchesGateway.insertGenre(id)
+            else -> throw IllegalArgumentException("invalid category ${mediaId.resolveId}")
+        }
+    }
+}
