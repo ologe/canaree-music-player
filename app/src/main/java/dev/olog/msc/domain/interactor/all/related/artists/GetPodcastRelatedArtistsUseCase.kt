9a109700fbbb26c6ea746d7dd77282a3e35d717b
Eugeniu Olog <eugeniu.olog@gmail.com>
2018-09-29 20:31:13 +0200

    1.2.5 - added podcast related artists

--- app/src/main/java/dev/olog/msc/domain/interactor/all/related/artists/GetPodcastRelatedArtistsUseCase.kt
+++ app/src/main/java/dev/olog/msc/domain/interactor/all/related/artists/GetPodcastRelatedArtistsUseCase.kt
@@ -1,34 +1,34 @@
 package dev.olog.msc.domain.interactor.all.related.artists
 
 import dev.olog.msc.constants.AppConstants
-import dev.olog.msc.domain.entity.Artist
+import dev.olog.msc.domain.entity.PodcastArtist
 import dev.olog.msc.domain.executors.ComputationScheduler
 import dev.olog.msc.domain.interactor.all.GetSongListByParamUseCase
 import dev.olog.msc.domain.interactor.base.ObservableUseCaseUseCaseWithParam
-import dev.olog.msc.domain.interactor.item.GetArtistUseCase
+import dev.olog.msc.domain.interactor.item.GetPodcastArtistUseCase
 import dev.olog.msc.utils.MediaId
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.toFlowable
 import java.text.Collator
 import javax.inject.Inject
 
-class GetRelatedArtistsUseCase @Inject constructor(
+class GetPodcastRelatedArtistsUseCase @Inject constructor(
         private val executors: ComputationScheduler,
         private val getSongListByParamUseCase: GetSongListByParamUseCase,
-        private val getArtistUseCase: GetArtistUseCase,
+        private val getPodcastArtistUseCase: GetPodcastArtistUseCase,
         private val collator: Collator
 
-) : ObservableUseCaseUseCaseWithParam<List<Artist>, MediaId>(executors) {
+) : ObservableUseCaseUseCaseWithParam<List<PodcastArtist>, MediaId>(executors) {
 
     @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
-    override fun buildUseCaseObservable(mediaId: MediaId): Observable<List<Artist>> {
-        if (mediaId.isFolder || mediaId.isPlaylist || mediaId.isGenre){
+    override fun buildUseCaseObservable(mediaId: MediaId): Observable<List<PodcastArtist>> {
+        if (mediaId.isPodcastPlaylist){
             return getSongListByParamUseCase.execute(mediaId)
                     .switchMapSingle { songList -> songList.toFlowable()
                             .filter { it.artist != AppConstants.UNKNOWN }
                             .distinct { it.artistId }
-                            .map { MediaId.artistId(it.artistId) }
-                            .flatMapSingle { getArtistUseCase.execute(it).firstOrError().subscribeOn(executors.worker) }
+                            .map { MediaId.podcastArtistId(it.artistId) }
+                            .flatMapSingle { getPodcastArtistUseCase.execute(it).firstOrError().subscribeOn(executors.worker) }
                             .toSortedList { o1, o2 -> collator.compare(o1.name, o2.name) }
                     }
         }
