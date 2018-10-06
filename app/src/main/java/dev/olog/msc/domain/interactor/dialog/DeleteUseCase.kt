f3fe1bd88f74eeceab0bd659f3deba9b643b642b
Eugeniu Olog <eugeniu.olog@energee3.com>
2018-10-06 12:59:28 +0200

    1.2.5 - fixed podcsat dialog use cases

--- app/src/main/java/dev/olog/msc/domain/interactor/dialog/DeleteUseCase.kt
+++ app/src/main/java/dev/olog/msc/domain/interactor/dialog/DeleteUseCase.kt
@@ -2,6 +2,7 @@ package dev.olog.msc.domain.interactor.dialog
 
 import dev.olog.msc.domain.executors.IoScheduler
 import dev.olog.msc.domain.gateway.PlaylistGateway
+import dev.olog.msc.domain.gateway.PodcastGateway
 import dev.olog.msc.domain.gateway.SongGateway
 import dev.olog.msc.domain.interactor.all.GetSongListByParamUseCase
 import dev.olog.msc.domain.interactor.base.CompletableUseCaseWithParam
@@ -12,6 +13,7 @@ import javax.inject.Inject
 class DeleteUseCase @Inject constructor(
         scheduler: IoScheduler,
         private val playlistGateway: PlaylistGateway,
+        private val podcastGateway: PodcastGateway,
         private val songGateway: SongGateway,
         private val getSongListByParamUseCase: GetSongListByParamUseCase
 
@@ -19,8 +21,12 @@ class DeleteUseCase @Inject constructor(
 
     @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
     override fun buildUseCaseObservable(mediaId: MediaId): Completable {
+        if (mediaId.isLeaf && mediaId.isPodcast){
+            return podcastGateway.deleteSingle(mediaId.resolveId)
+        }
+
         if (mediaId.isLeaf) {
-            return songGateway.deleteSingle(mediaId.leaf!!)
+            return songGateway.deleteSingle(mediaId.resolveId)
         }
 
         return when {
--- app/src/main/java/dev/olog/msc/domain/interactor/dialog/RenameUseCase.kt
+++ app/src/main/java/dev/olog/msc/domain/interactor/dialog/RenameUseCase.kt
@@ -2,6 +2,7 @@ package dev.olog.msc.domain.interactor.dialog
 
 import dev.olog.msc.domain.executors.IoScheduler
 import dev.olog.msc.domain.gateway.PlaylistGateway
+import dev.olog.msc.domain.gateway.PodcastPlaylistGateway
 import dev.olog.msc.domain.interactor.base.CompletableUseCaseWithParam
 import dev.olog.msc.utils.MediaId
 import io.reactivex.Completable
@@ -10,7 +11,7 @@ import javax.inject.Inject
 class RenameUseCase @Inject constructor(
         scheduler: IoScheduler,
         private val playlistGateway: PlaylistGateway,
-        private val podcastPlaylistGateway: PlaylistGateway
+        private val podcastPlaylistGateway: PodcastPlaylistGateway
 
 ) : CompletableUseCaseWithParam<Pair<MediaId, String>>(scheduler) {
 
@@ -18,7 +19,7 @@ class RenameUseCase @Inject constructor(
     override fun buildUseCaseObservable(param: Pair<MediaId, String>): Completable {
         val (mediaId, newTitle) = param
         return when {
-            mediaId.isPodcast -> podcastPlaylistGateway.renamePlaylist(mediaId.categoryValue.toLong(), newTitle)
+            mediaId.isPodcastPlaylist -> podcastPlaylistGateway.renamePlaylist(mediaId.categoryValue.toLong(), newTitle)
             mediaId.isPlaylist -> playlistGateway.renamePlaylist(mediaId.categoryValue.toLong(), newTitle)
             else -> Completable.error(IllegalArgumentException("not a folder nor a playlist, $mediaId"))
         }
