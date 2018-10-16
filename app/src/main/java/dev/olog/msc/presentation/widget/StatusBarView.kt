0ab3fbcdaf460c6a2950d1153c2741cfee82a98f
eugeniuolog <D3aadd9b44>
2018-10-17 00:50:21 +0200

    2.0.3 - fixed notch

--- app/src/main/java/dev/olog/msc/presentation/widget/StatusBarView.kt
+++ app/src/main/java/dev/olog/msc/presentation/widget/StatusBarView.kt
@@ -1,16 +1,12 @@
 package dev.olog.msc.presentation.widget
 
 import android.content.Context
-import android.os.Build
-import android.preference.PreferenceManager
 import android.util.AttributeSet
 import android.view.View
 import dev.olog.msc.R
-import dev.olog.msc.utils.isP
 import dev.olog.msc.utils.k.extension.dimen
 import dev.olog.msc.utils.k.extension.dip
 import dev.olog.msc.utils.k.extension.hasNotch
-import dev.olog.msc.utils.k.extension.isPortrait
 
 /**
  * Custom status bar to handle device notch
@@ -25,10 +21,9 @@ class StatusBarView @JvmOverloads constructor(
     private val statusBarHeightPlusNotch = context.dip(48)
     private var hasNotch = false
 
-    init {
-        if (context.isPortrait && !isInEditMode){
-            hasNotch = this.hasNotch()
-        }
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        hasNotch = this.hasNotch()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
