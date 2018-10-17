5e14102e35eefddcbda7c228d8a6d88a0d0110c0
eugeniuolog <D3aadd9b44>
2018-10-17 20:45:34 +0200

    2.0.3 - fixed add to playlist not working

--- app/src/main/java/dev/olog/msc/presentation/dialog/create/playlist/NewPlaylistDialogPresenter.kt
+++ app/src/main/java/dev/olog/msc/presentation/dialog/create/playlist/NewPlaylistDialogPresenter.kt
@@ -2,16 +2,23 @@ package dev.olog.msc.presentation.dialog.create.playlist
 
 import dev.olog.msc.domain.entity.PlaylistType
 import dev.olog.msc.domain.interactor.all.GetPlaylistsBlockingUseCase
+import dev.olog.msc.domain.interactor.all.GetSongListByParamUseCase
+import dev.olog.msc.domain.interactor.item.GetPodcastUseCase
+import dev.olog.msc.domain.interactor.item.GetSongUseCase
 import dev.olog.msc.domain.interactor.playlist.InsertCustomTrackListRequest
 import dev.olog.msc.domain.interactor.playlist.InsertCustomTrackListToPlaylist
 import dev.olog.msc.utils.MediaId
+import dev.olog.msc.utils.k.extension.mapToList
 import io.reactivex.Completable
 import javax.inject.Inject
 
 class NewPlaylistDialogPresenter @Inject constructor(
         private val mediaId: MediaId,
         playlists: GetPlaylistsBlockingUseCase,
-        private val insertCustomTrackListToPlaylist: InsertCustomTrackListToPlaylist
+        private val insertCustomTrackListToPlaylist: InsertCustomTrackListToPlaylist,
+        private val getSongListByParamUseCase: GetSongListByParamUseCase,
+        private val getSongUseCase: GetSongUseCase,
+        private val getPocastUseCase: GetPodcastUseCase
 
 ) {
 
@@ -21,8 +28,22 @@ class NewPlaylistDialogPresenter @Inject constructor(
             .map { it.title.toLowerCase() }
 
     fun execute(playlistTitle: String) : Completable {
-        return insertCustomTrackListToPlaylist.execute(InsertCustomTrackListRequest(playlistTitle,
-                listOf(mediaId.resolveId), playlistType))
+        return if (mediaId.isLeaf && mediaId.isPodcast) {
+            getPocastUseCase.execute(mediaId).firstOrError().map { listOf(it.id) }
+                    .flatMapCompletable {
+                        insertCustomTrackListToPlaylist.execute(InsertCustomTrackListRequest(playlistTitle, it, playlistType))
+                    }
+        } else if (mediaId.isLeaf) {
+            getSongUseCase.execute(mediaId).firstOrError().map { listOf(it.id) }
+                    .flatMapCompletable {
+                        insertCustomTrackListToPlaylist.execute(InsertCustomTrackListRequest(playlistTitle, it, playlistType))
+                    }
+        } else {
+            getSongListByParamUseCase.execute(mediaId).firstOrError().mapToList { it.id }
+                    .flatMapCompletable {
+                        insertCustomTrackListToPlaylist.execute(InsertCustomTrackListRequest(playlistTitle, it, playlistType))
+                    }
+        }
     }
 
     fun isStringValid(string: String): Boolean {
