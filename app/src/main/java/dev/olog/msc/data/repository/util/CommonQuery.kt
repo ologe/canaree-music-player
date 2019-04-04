e8a8712cdccefa0efff3137ab2f509199777b4f6
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-04-04 19:51:36 +0200

    2.1.1 - crash fix, sometimes column count(*) is not found

--- app/src/main/java/dev/olog/msc/data/repository/util/CommonQuery.kt
+++ app/src/main/java/dev/olog/msc/data/repository/util/CommonQuery.kt
@@ -14,18 +14,20 @@ import java.io.File
 object CommonQuery {
 
     fun getSize(contentResolver: ContentResolver, uri: Uri): Int {
+        try {
+            val cursor = contentResolver.query(uri, arrayOf("count(*)"), null,
+                    null, null)
 
-        val cursor = contentResolver.query(uri, arrayOf("count(*)"), null,
-                null, null)
+            var size = 0
+            cursor?.use {
+                it.moveToFirst()
+                size = cursor.getInt(0)
+            }
 
-        var size = 0
-        cursor?.use {
-            it.moveToFirst()
-            size = cursor.getInt(0)
+            return size
+        } catch (ex: IllegalArgumentException) {
+            return 0
         }
-
-        return size
-
     }
 
     fun extractAlbumIdsFromSongs(contentResolver: ContentResolver, uri: Uri): List<Long> {
@@ -37,7 +39,7 @@ object CommonQuery {
                 null, null)
 
         cursor?.use {
-            while (cursor.moveToNext()){
+            while (cursor.moveToNext()) {
                 result.add(it.getLong(0))
             }
         }
@@ -55,7 +57,7 @@ object CommonQuery {
                 "${MediaStore.Audio.Media.IS_PODCAST} = 0", null, null)
 
         cursor?.use {
-            while (it.moveToNext()){
+            while (it.moveToNext()) {
                 list.add(it.getLong(0) to it.getString(1))
             }
         }
@@ -63,7 +65,7 @@ object CommonQuery {
         return removeBlacklisted(appPreferencesUseCase.getBlackList(), list)
     }
 
-    private fun removeBlacklisted(blackList: Set<String>, original: List<Pair<Long, String>>): List<Long>{
+    private fun removeBlacklisted(blackList: Set<String>, original: List<Pair<Long, String>>): List<Long> {
         return original
                 .asSequence()
                 .filter {
@@ -82,9 +84,9 @@ object CommonQuery {
         val result = SparseArray<String>()
 
         cursor?.use {
-            while (it.moveToNext()){
+            while (it.moveToNext()) {
                 val albumArt = it.getStringOrNull(MediaStore.Audio.Albums.ALBUM_ART)
-                if (albumArt != null){
+                if (albumArt != null) {
                     val id = cursor.getInt(MediaStore.Audio.Albums._ID)
                     result.append(id, albumArt)
                 }
