cefd6435b142d447112ff08f9367bf8cc2212254
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-06-21 12:55:30 +0200

    removed fragment analytics

--- app/src/main/java/dev/olog/msc/presentation/base/BaseDialogFragment.kt
+++ app/src/main/java/dev/olog/msc/presentation/base/BaseDialogFragment.kt
@@ -4,18 +4,12 @@ import android.content.Context
 import androidx.annotation.CallSuper
 import androidx.fragment.app.DialogFragment
 import dagger.android.support.AndroidSupportInjection
-import dev.olog.msc.analytics.AppAnalytics
 
-abstract class BaseDialogFragment : DialogFragment(), LoggableFragment {
+abstract class BaseDialogFragment : DialogFragment() {
 
     @CallSuper
     override fun onAttach(context: Context) {
         AndroidSupportInjection.inject(this)
         super.onAttach(context)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        AppAnalytics.logScreen(activity, this)
     }
 }
