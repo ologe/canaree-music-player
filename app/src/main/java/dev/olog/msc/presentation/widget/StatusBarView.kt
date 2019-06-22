4ca6e499d4ea988ac700efe18ab05ec1788c336c
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-06-22 17:44:52 +0200

    cleaning

--- app/src/main/java/dev/olog/msc/presentation/widget/StatusBarView.kt
+++ app/src/main/java/dev/olog/msc/presentation/widget/StatusBarView.kt
@@ -4,23 +4,19 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.view.WindowInsets
-import dev.olog.msc.R
-import dev.olog.msc.presentation.theme.AppTheme
-import dev.olog.msc.utils.k.extension.dimen
-import dev.olog.msc.utils.k.extension.dip
-import dev.olog.msc.utils.k.extension.hasNotch
-import dev.olog.msc.utils.k.extension.isPortrait
 import dev.olog.shared.colorSurface
 import dev.olog.shared.setHeight
 
 /**
  * Custom status bar to handle device notch
  */
-class StatusBarView : View {
+class StatusBarView(
+    context: Context,
+    attrs: AttributeSet
+) : View(context, attrs) {
 
-    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        init()
+    init {
+        setBackgroundColor(context.colorSurface())
     }
 
     private var viewHeight = 0
@@ -29,10 +25,6 @@ class StatusBarView : View {
         viewHeight = insets?.systemWindowInsetTop ?: 0
         setHeight(viewHeight)
         return super.onApplyWindowInsets(insets)
-    }
-
-    private fun init() {
-        setBackgroundColor(context.colorSurface())
     }
 
 }
