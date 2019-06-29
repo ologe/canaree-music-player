0739654085c44fdd39aca834599f84605ed4a1cf
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-06-29 22:30:20 +0200

    fixed error message

--- shared/src/main/java/dev/olog/shared/utils/ThreadUtils.kt
+++ shared/src/main/java/dev/olog/shared/utils/ThreadUtils.kt
@@ -8,25 +8,20 @@ private val handler = Handler(Looper.getMainLooper())
 fun isMainThread() = Looper.myLooper() == Looper.getMainLooper()
 
 fun assertMainThread() {
-    if (BuildConfig.DEBUG) {
-        val isMainThread = isMainThread()
-        if (!isMainThread) {
-            throw AssertionError("not on main thread " + Thread.currentThread())
-        }
+    if (BuildConfig.DEBUG && !isMainThread()) {
+        throw AssertionError("not on main thread, current=${Thread.currentThread()}")
+
     }
 }
 
 fun assertBackgroundThread() {
-    if (BuildConfig.DEBUG) {
-        val isBackgroundThread = !isMainThread()
-        if (!isBackgroundThread) {
-            throw AssertionError("not on worker thread " + Thread.currentThread())
-        }
+    if (BuildConfig.DEBUG && isMainThread()) {
+        throw AssertionError("not on worker thread, current=${Thread.currentThread()}")
     }
 }
 
-fun runOnMainThread(func : () -> Unit){
-    if (isMainThread()){
+fun runOnMainThread(func: () -> Unit) {
+    if (isMainThread()) {
         func()
     } else {
         handler.post(func)
