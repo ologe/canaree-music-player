f72e4f29473a1b2bc86da9536f202e5e302f0c1f
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-06-29 23:14:42 +0200

    function name update

--- app/src/test/java/dev/olog/msc/ClassTest.kt
+++ app/src/test/java/dev/olog/msc/ClassTest.kt
@@ -3,7 +3,7 @@ package dev.olog.msc
 import dev.olog.appshortcuts.ShortcutsActivity
 import dev.olog.service.floating.FloatingWindowService
 import dev.olog.service.music.MusicService
-import dev.olog.msc.presentation.app.widget.WidgetColored
+import dev.olog.msc.appwidgets.WidgetColored
 import dev.olog.presentation.main.MainActivity
 import dev.olog.msc.presentation.shortcuts.playlist.chooser.PlaylistChooserActivity
 import dev.olog.shared.Classes
@@ -13,7 +13,7 @@ import org.junit.Test
 class ClassTest {
 
     @Test
-    fun checkMusicServiceExistence(){
+    fun checkClassExistence(){
         // activities
         Assert.assertEquals(Classes.ACTIVITY_MAIN, MainActivity::class.java.name)
         Assert.assertEquals(Classes.ACTIVITY_SHORTCUTS, ShortcutsActivity::class.java.name)
