17cb26ee782cfc53388673648a8f2070227bfbd5
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-06-29 23:45:25 +0200

    removed unused classes

--- core/src/main/java/dev/olog/core/entity/LastFmEntities.kt
+++ core/src/main/java/dev/olog/core/entity/LastFmEntities.kt
@@ -19,23 +19,3 @@ data class LastFmArtist(
         val id: Long,
         val image: String
 )
-
-data class LastFmPodcast(
-        val id: Long,
-        val title: String,
-        val artist: String,
-        val album: String,
-        val image: String
-)
-
-data class LastFmPodcastAlbum(
-        val id: Long,
-        val title: String,
-        val artist: String,
-        val image: String
-)
-
-data class LastFmPodcastArtist(
-        val id: Long,
-        val image: String
-)
