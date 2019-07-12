a0ebc0429f2566732ee50e404908c0a33ab3ece9
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-07-12 03:11:32 +0200

    added current song test class

---
+++ service-music/src/test/java/dev/olog/service/music/CurrentSongTest.kt
@@ -0,0 +1,13 @@
+package dev.olog.service.music
+
+import dev.olog.test.shared.CoroutinesMainDispatcherRule
+import org.junit.Rule
+
+class CurrentSongTest {
+
+    @get:Rule
+    var coroutinesMainDispatcherRule = CoroutinesMainDispatcherRule()
+
+//    private val currentSong = CurrentSong()
+
+}
