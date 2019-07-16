9dec7e5da8b9f7d506a54bae716397a35a284ff7
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-07-16 14:46:21 +0200

    set navigator bar to follow colorSurface event on dark mode

--- presentation/src/main/java/dev/olog/presentation/utils/WindowExtension.kt
+++ presentation/src/main/java/dev/olog/presentation/utils/WindowExtension.kt
@@ -8,47 +8,53 @@ import dev.olog.shared.theme.isImmersiveMode
 import dev.olog.shared.utils.isMarshmallow
 import dev.olog.shared.utils.isOreo
 
-fun Window.setLightStatusBar(){
+fun Window.setLightStatusBar() {
     decorView.systemUiVisibility = 0
 
     var flags = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
 
-    if (context.isImmersiveMode()){
+    if (context.isImmersiveMode()) {
         flags = flags or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
     }
 
-    if (isMarshmallow() && !context.isDarkMode()){
+    val isDarkMode = context.isDarkMode()
+    if (isDarkMode) {
+        navigationBarColor = context.colorSurface()
+    }
+
+    if (isMarshmallow() && !isDarkMode) {
         flags = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 
-        if (isOreo()){
+        if (isOreo()) {
             navigationBarColor = context.colorSurface()
-            if (!context.isDarkMode()){
-                flags = flags or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-            }
+            flags = flags or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
         }
     }
     decorView.systemUiVisibility = flags
 }
 
-fun Window.removeLightStatusBar(){
+fun Window.removeLightStatusBar() {
     decorView.systemUiVisibility = 0
 
     var flags = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
 
-    if (context.isImmersiveMode()){
+    if (context.isImmersiveMode()) {
         flags = flags or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
     }
 
-    if (isMarshmallow() && !context.isDarkMode()){
-        if (isOreo()){
+    val isDarkMode = context.isDarkMode()
+    if (isDarkMode) {
+        navigationBarColor = context.colorSurface()
+    }
+
+    if (isMarshmallow() && !isDarkMode) {
+        if (isOreo()) {
             navigationBarColor = context.colorSurface()
-            if (!context.isDarkMode()){
-                flags = flags or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-            }
+            flags = flags or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
         }
     }
     decorView.systemUiVisibility = flags
