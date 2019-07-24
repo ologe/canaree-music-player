f9773a61483f48db302e927bd96a48632340040f
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-07-24 22:15:37 +0200

    deleted unused code

--- app/src/main/java/dev/olog/msc/utils/k/extension/ViewModelExtension.kt
+++
@@ -1,7 +0,0 @@
-package dev.olog.msc.utils.k.extension
-
-import android.content.Context
-import androidx.lifecycle.AndroidViewModel
-
-val AndroidViewModel.context : Context
-    get() = getApplication()
