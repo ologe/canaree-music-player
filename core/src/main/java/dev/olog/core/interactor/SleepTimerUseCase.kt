03d9dc56439f9d8d52f18a256311484784f72758
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-07-25 22:31:31 +0200

    removed core module dependency on shared module

--- core/src/main/java/dev/olog/core/interactor/SleepTimerUseCase.kt
+++ core/src/main/java/dev/olog/core/interactor/SleepTimerUseCase.kt
@@ -1,16 +1,22 @@
 package dev.olog.core.interactor
 
 import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import dev.olog.core.dagger.ApplicationContext
 import dev.olog.core.prefs.AppPreferencesGateway
-import dev.olog.shared.PendingIntents
 import javax.inject.Inject
 
 class SleepTimerUseCase @Inject constructor(
     @ApplicationContext private val context: Context,
     private val gateway: AppPreferencesGateway
 ) {
+
+    companion object {
+        const val ACTION_STOP_SLEEP_END = "action.stop_sleep_timer"
+    }
 
     fun getLast(): SleepData = SleepData(
         gateway.getSleepFrom(),
@@ -20,7 +26,7 @@ class SleepTimerUseCase @Inject constructor(
     fun set(sleepFrom: Long, sleepTime: Long, nextSleep: Long) {
         gateway.setSleepTimer(sleepFrom, sleepTime)
 
-        val intent = PendingIntents.stopMusicServiceIntent(context)
+        val intent = stopMusicServiceIntent(context)
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, nextSleep, intent)
     }
@@ -28,7 +34,20 @@ class SleepTimerUseCase @Inject constructor(
     fun reset() {
         gateway.resetSleepTimer()
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        alarmManager.cancel(PendingIntents.stopMusicServiceIntent(context))
+        alarmManager.cancel(stopMusicServiceIntent(context))
+    }
+
+    private fun stopMusicServiceIntent(context: Context): PendingIntent {
+        val intent = Intent(context, Class.forName("dev.olog.service.music.MusicService"))
+        intent.action = ACTION_STOP_SLEEP_END
+        return intent.asServicePendingIntent(context, PendingIntent.FLAG_CANCEL_CURRENT)
+    }
+
+    private fun Intent.asServicePendingIntent(context: Context, flag: Int = PendingIntent.FLAG_CANCEL_CURRENT): PendingIntent{
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            return PendingIntent.getForegroundService(context, 0, this, flag)
+        }
+        return PendingIntent.getService(context, 0, this, flag)
     }
 
 }
--- shared/src/main/java/dev/olog/shared/utils/ThreadUtils.kt
+++ shared/src/main/java/dev/olog/shared/utils/ThreadUtils.kt
@@ -1,3 +1,5 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package dev.olog.shared.utils
 
 import android.os.Handler
