9aa79dd0db9dc78e19bdc82542509d26c28bcf11
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-07-26 08:17:56 +0200

    network check fix

--- shared-android/src/main/java/dev/olog/shared/android/utils/NetworkUtils.kt
+++ shared-android/src/main/java/dev/olog/shared/android/utils/NetworkUtils.kt
@@ -7,21 +7,22 @@ import android.net.wifi.WifiManager
 @Suppress("DEPRECATION")
 object NetworkUtils {
 
+    private val allowedConnectionTypes = listOf(
+        ConnectivityManager.TYPE_WIFI,
+        ConnectivityManager.TYPE_WIMAX,
+        ConnectivityManager.TYPE_ETHERNET
+    )
+
     @JvmStatic
     fun isConnected(context: Context): Boolean {
         val manager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetworkInfo = manager.activeNetworkInfo ?: return false
-        val isWifi = activeNetworkInfo.type == ConnectivityManager.TYPE_WIFI
-        val isMobile = activeNetworkInfo.type == ConnectivityManager.TYPE_MOBILE
-        return isWifi || isMobile
+        return manager.activeNetworkInfo?.isConnectedOrConnecting == true
     }
 
     @JvmStatic
     fun isOnWiFi(context: Context): Boolean {
-        val wifiManager = context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager?
-
-        return wifiManager?.isWifiEnabled ?: false &&
-                wifiManager?.connectionInfo?.networkId != -1
+        val manager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return isConnected(context) && manager.activeNetworkInfo?.type in allowedConnectionTypes
     }
 
 }
