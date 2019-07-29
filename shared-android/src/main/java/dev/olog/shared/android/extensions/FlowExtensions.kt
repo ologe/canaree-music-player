c47fbe03b4bdbb6b195639eacedd4475f6337127
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-07-29 04:01:54 +0200

    crash fix

--- shared-android/src/main/java/dev/olog/shared/android/extensions/FlowExtensions.kt
+++ shared-android/src/main/java/dev/olog/shared/android/extensions/FlowExtensions.kt
@@ -2,13 +2,14 @@ package dev.olog.shared.android.extensions
 
 import dev.olog.shared.android.utils.assertBackgroundThread
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 
 @Suppress("NOTHING_TO_INLINE")
 inline fun <T> Flow<T>.assertBackground(): Flow<T> {
-    return flow {
+    return channelFlow {
         assertBackgroundThread()
-        collect { emit(it) }
+        collect { offer(it) }
     }
 }
