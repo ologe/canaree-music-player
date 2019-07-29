305e35b625f2d40342c4b5dba564824ccb366335
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-07-29 21:42:59 +0200

    added login check in last fm credential

--- presentation/src/main/java/dev/olog/presentation/prefs/lastfm/LastFmCredentialsFragment.kt
+++ presentation/src/main/java/dev/olog/presentation/prefs/lastfm/LastFmCredentialsFragment.kt
@@ -1,21 +1,26 @@
 package dev.olog.presentation.prefs.lastfm
 
 import android.app.Dialog
+import android.app.ProgressDialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.EditText
-import androidx.appcompat.app.AlertDialog
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import de.umass.lastfm.Authenticator
 import dev.olog.core.entity.UserCredentials
 import dev.olog.core.interactor.lastfm.GetLastFmUserCredentials
 import dev.olog.core.interactor.lastfm.UpdateLastFmUserCredentials
+import dev.olog.presentation.BuildConfig
 import dev.olog.presentation.R
 import dev.olog.presentation.base.BaseDialogFragment
 import dev.olog.shared.android.extensions.ctx
+import dev.olog.shared.android.extensions.toast
+import kotlinx.coroutines.*
 import javax.inject.Inject
 
-class LastFmCredentialsFragment : BaseDialogFragment() {
+class LastFmCredentialsFragment : BaseDialogFragment(), CoroutineScope by MainScope() {
 
     companion object {
         const val TAG = "LastFmCredentialsFragment"
@@ -31,11 +36,15 @@ class LastFmCredentialsFragment : BaseDialogFragment() {
     @Inject
     lateinit var updateLastFmUserCredentials: UpdateLastFmUserCredentials
 
+    private var loader: ProgressDialog? = null
+
+    private var job: Job? = null
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val inflater = LayoutInflater.from(activity!!)
         val view: View = inflater.inflate(R.layout.fragment_credentials, null, false)
 
-        val builder = AlertDialog.Builder(ctx)
+        val builder = MaterialAlertDialogBuilder(ctx)
             .setTitle(R.string.prefs_last_fm_credentials_title)
             .setMessage(R.string.prefs_last_fm_credentials_message)
             .setView(view)
@@ -52,16 +61,63 @@ class LastFmCredentialsFragment : BaseDialogFragment() {
         val dialog = builder.show()
 
         dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
-            val user = UserCredentials(
-                userName.text.toString(),
-                password.text.toString()
-            )
-            TODO()
-//            val disp = updateLastFmUserCredentials(user)
-//                .subscribe({ dismiss() }, Throwable::printStackTrace)
+            job = launch {
+
+                val user = UserCredentials(
+                    userName.text.toString(),
+                    password.text.toString()
+                )
+                showLoader()
+                try {
+                    if (tryAuthenticate(user)) {
+                        onSuccess(user)
+                    } else {
+                        onFail()
+                    }
+                } catch (ex: Throwable) {
+                    onFail()
+                } finally {
+                    loader?.dismiss()
+                }
+            }
+
         }
 
         return dialog
+    }
+
+    private fun showLoader() {
+        loader = ProgressDialog.show(context, "", "Authenticating", true).apply {
+            setCancelable(true)
+            setCanceledOnTouchOutside(true)
+            setOnCancelListener {
+                setOnCancelListener {
+                    job?.cancel()
+                    loader = null
+                }
+            }
+        }
+    }
+
+    private suspend fun tryAuthenticate(user: UserCredentials): Boolean =
+        withContext(Dispatchers.IO) {
+            Authenticator.getMobileSession(
+                user.username,
+                user.password,
+                BuildConfig.LAST_FM_KEY,
+                BuildConfig.LAST_FM_SECRET
+            ) != null
+
+        }
+
+    private fun onSuccess(user: UserCredentials) {
+        updateLastFmUserCredentials(user)
+        ctx.toast("Success")
+        dismiss()
+    }
+
+    private fun onFail() {
+        ctx.toast("Failed")
     }
 
 }
