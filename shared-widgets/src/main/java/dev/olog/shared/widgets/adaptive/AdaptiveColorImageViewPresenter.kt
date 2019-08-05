303592bd6ec1b2802a253289da057b6360f41769
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-08-05 18:22:10 +0200

    added desaturation to adaptive colors

--- shared-widgets/src/main/java/dev/olog/shared/widgets/adaptive/AdaptiveColorImageViewPresenter.kt
+++ shared-widgets/src/main/java/dev/olog/shared/widgets/adaptive/AdaptiveColorImageViewPresenter.kt
@@ -2,13 +2,13 @@ package dev.olog.shared.widgets.adaptive
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.Color
 import android.graphics.drawable.Drawable
+import androidx.core.graphics.ColorUtils
 import androidx.core.graphics.drawable.toBitmap
+import androidx.core.math.MathUtils
 import androidx.palette.graphics.Palette
-import dev.olog.shared.android.extensions.colorAccent
-import dev.olog.shared.android.extensions.colorBackground
-import dev.olog.shared.android.extensions.textColorPrimary
-import dev.olog.shared.android.extensions.textColorSecondary
+import dev.olog.shared.android.extensions.*
 import dev.olog.shared.android.palette.ColorUtil
 import dev.olog.shared.android.palette.ImageProcessor
 import kotlinx.coroutines.*
@@ -20,13 +20,17 @@ class AdaptiveColorImageViewPresenter(
     private val context: Context
 ) {
 
-    private val defaultProcessorColors =
-        ValidProcessorColors(
-            context.colorBackground(), context.textColorPrimary(), context.textColorSecondary()
-        )
+    private val isDarkMode by lazy(LazyThreadSafetyMode.NONE) {
+        context.isDarkMode()
+    }
 
-    private val defaultPaletteColors =
-        ValidPaletteColors(context.colorAccent())
+    private val defaultProcessorColors = ValidProcessorColors(
+        context.colorBackground(),
+        context.textColorPrimary(),
+        context.textColorSecondary()
+    )
+
+    private val defaultPaletteColors = ValidPaletteColors(context.colorAccent())
 
     private val processorPalettePublisher = ConflatedBroadcastChannel(defaultProcessorColors)
     private val palettePublisher = ConflatedBroadcastChannel(defaultPaletteColors)
@@ -57,7 +61,11 @@ class AdaptiveColorImageViewPresenter(
             val image = ImageProcessor(context).processImage(bitmap)
             yield()
             processorPalettePublisher.offer(
-                ValidProcessorColors(image.background, image.primaryTextColor, image.secondaryTextColor)
+                ValidProcessorColors(
+                    desaturate(image.background),
+                    desaturate(image.primaryTextColor),
+                    desaturate(image.secondaryTextColor)
+                )
             )
         }
 
@@ -66,9 +74,33 @@ class AdaptiveColorImageViewPresenter(
                 .maximumColorCount(24)
                 .generate()
             yield()
-            val accent = ColorUtil.getAccentColor(context, palette)
+            val accent = desaturate(ColorUtil.getAccentColor(context, palette))
             palettePublisher.offer(ValidPaletteColors(accent))
         }
+    }
+
+    private fun desaturate(color: Int): Int {
+        if (!isDarkMode){
+            return color
+        }
+
+        if (color == Color.TRANSPARENT) {
+            // can't desaturate transparent color
+            return color
+        }
+        val amount = .25f
+        val minDesaturation = .75f
+
+        val hsl = floatArrayOf(0f, 0f, 0f)
+        ColorUtils.colorToHSL(color, hsl)
+        if (hsl[1] > minDesaturation) {
+            hsl[1] = MathUtils.clamp(
+                hsl[1] - amount,
+                minDesaturation - 0.1f,
+                1f
+            )
+        }
+        return ColorUtils.HSLToColor(hsl)
     }
 
 }
