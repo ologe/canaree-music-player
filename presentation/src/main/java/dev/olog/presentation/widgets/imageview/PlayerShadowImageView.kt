fd1277b8976c4970776ac5cca3c257680662c3e4
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-08-09 17:46:39 +0200

    crash fix

--- presentation/src/main/java/dev/olog/presentation/widgets/imageview/PlayerShadowImageView.kt
+++ presentation/src/main/java/dev/olog/presentation/widgets/imageview/PlayerShadowImageView.kt
@@ -6,6 +6,7 @@ import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import androidx.core.content.ContextCompat
+import androidx.core.view.doOnLayout
 import androidx.core.view.doOnPreDraw
 import dev.olog.presentation.R
 import dev.olog.presentation.widgets.imageview.shape.ShapeImageView
@@ -82,7 +83,7 @@ class PlayerShadowImageView(
 
     private inline fun setBlurShadow(crossinline setImage: () -> Unit = {}) {
         background = null
-        doOnPreDraw {
+        doOnLayout {
             setImage()
             makeBlurShadow()
         }
