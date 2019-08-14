c509397ab2faec0f4d60f1c2f3790e99b5310f3e
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-08-14 21:22:29 +0200

    memory leak fix

--- core/src/main/java/dev/olog/core/entity/favorite/FavoriteStateEntity.kt
+++ core/src/main/java/dev/olog/core/entity/favorite/FavoriteStateEntity.kt
@@ -1,34 +1,13 @@
 package dev.olog.core.entity.favorite
 
-class FavoriteStateEntity(
+data class FavoriteStateEntity(
     @JvmField
     val songId: Long,
     @JvmField
     val enum: FavoriteEnum,
     @JvmField
     val favoriteType: FavoriteType
-) {
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as FavoriteStateEntity
-
-        if (songId != other.songId) return false
-        if (enum != other.enum) return false
-        if (favoriteType != other.favoriteType) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = songId.hashCode()
-        result = 31 * result + enum.hashCode()
-        result = 31 * result + favoriteType.hashCode()
-        return result
-    }
-}
+)
 
 enum class FavoriteEnum {
     FAVORITE,
