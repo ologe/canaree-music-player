4c93ea9830aac572f3ec6cea2ff3cc9618470c26
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-08-14 20:42:35 +0200

    leaf fix

--- presentation/src/main/java/dev/olog/presentation/player/PlayerFragmentPresenter.kt
+++ presentation/src/main/java/dev/olog/presentation/player/PlayerFragmentPresenter.kt
@@ -4,15 +4,20 @@ import dev.olog.presentation.model.PresentationPreferencesGateway
 import dev.olog.presentation.pro.IBilling
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.map
+import java.lang.ref.WeakReference
 import javax.inject.Inject
 
 internal class PlayerFragmentPresenter @Inject constructor(
-    private val billing: IBilling,
+    billing: IBilling,
     private val appPrefsUseCase: PresentationPreferencesGateway
 ) {
 
+    private val billingRef = WeakReference(billing)
+
     fun observePlayerControlsVisibility(): Flow<Boolean> {
+        val billing = billingRef.get() ?: return emptyFlow()
         return billing.observeBillingsState().map { it.isPremiumEnabled() }
             .combine(appPrefsUseCase.observePlayerControlsVisibility())
             { premium, show -> premium && show }
