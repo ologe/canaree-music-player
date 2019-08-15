71a56de986a93adaca432185449f1c275f57cc3a
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-08-15 02:46:35 +0200

    crash fix

--- presentation/src/main/java/dev/olog/presentation/prefs/blacklist/BlacklistFragmentPresenter.kt
+++ presentation/src/main/java/dev/olog/presentation/prefs/blacklist/BlacklistFragmentPresenter.kt
@@ -60,7 +60,14 @@ class BlacklistModel(
 
     // show the path without "/storage/emulated/0"
     @JvmField
-    val displayablePath = path.substring(defaultStorageDir.length)
+    val displayablePath = {
+        try {
+            path.substring(defaultStorageDir.length)
+        } catch (ex: StringIndexOutOfBoundsException){
+            ex.printStackTrace()
+            path
+        }
+    }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
