55c371a425dca943bba5b55a58b0b9fd943aa191
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-08-17 00:16:24 +0200

    cleaning

--- image-provider/src/main/java/dev/olog/image/provider/loader/GlideOriginalImageLoader.kt
+++ image-provider/src/main/java/dev/olog/image/provider/loader/GlideOriginalImageLoader.kt
@@ -1,7 +1,6 @@
 package dev.olog.image.provider.loader
 
 import android.content.Context
-import android.net.Uri
 import com.bumptech.glide.load.Options
 import com.bumptech.glide.load.model.ModelLoader
 import com.bumptech.glide.load.model.ModelLoaderFactory
@@ -16,24 +15,20 @@ import javax.inject.Inject
 
 internal class GlideOriginalImageLoader(
     private val context: Context,
-    private val uriLoader: ModelLoader<Uri, InputStream>,
     private val songGateway: SongGateway,
     private val podcastGateway: PodcastGateway
 
 ) : ModelLoader<MediaId, InputStream> {
 
     override fun handles(mediaId: MediaId): Boolean {
-        if (mediaId.isArtist) {
-            // artist image never exists on device
-            return false
-        }
-        if (mediaId.isLeaf){
+        if (mediaId.isLeaf) {
             return true
         }
-        if (mediaId.isFolder || mediaId.isPlaylist || mediaId.isGenre || mediaId.isPodcastPlaylist) {
-            return false
+        if (mediaId.isAlbum || mediaId.isPodcastAlbum) {
+            return true
         }
-        return true
+
+        return false
     }
 
     override fun buildLoadData(
@@ -43,19 +38,16 @@ internal class GlideOriginalImageLoader(
         options: Options
     ): ModelLoader.LoadData<InputStream>? {
 
-        if (mediaId.isAlbum || mediaId.isPodcastAlbum || mediaId.isLeaf) {
-            // retrieve image store on track
-            return ModelLoader.LoadData(
-                MediaIdKey(mediaId),
-                GlideOriginalImageFetcher(
-                    context,
-                    mediaId,
-                    songGateway,
-                    podcastGateway
-                )
+        // retrieve image store on track
+        return ModelLoader.LoadData(
+            MediaIdKey(mediaId),
+            GlideOriginalImageFetcher(
+                context,
+                mediaId,
+                songGateway,
+                podcastGateway
             )
-        }
-        return uriLoader.buildLoadData(Uri.EMPTY, width, height, options)
+        )
     }
 
     class Factory @Inject constructor(
@@ -66,10 +58,8 @@ internal class GlideOriginalImageLoader(
     ) : ModelLoaderFactory<MediaId, InputStream> {
 
         override fun build(multiFactory: MultiModelLoaderFactory): ModelLoader<MediaId, InputStream> {
-            val uriLoader = multiFactory.build(Uri::class.java, InputStream::class.java)
             return GlideOriginalImageLoader(
                 context,
-                uriLoader,
                 songGateway,
                 podcastGateway
             )
