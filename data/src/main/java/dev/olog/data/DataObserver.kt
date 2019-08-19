a47559334e0b768f5e2b17b3fbd8ae8453faadc3
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-08-19 16:48:20 +0200

    restored from previous commit, made 'onUpdate' be called on a background thread since the callback is making an actual query

--- data/src/main/java/dev/olog/data/DataObserver.kt
+++ data/src/main/java/dev/olog/data/DataObserver.kt
@@ -3,13 +3,16 @@ package dev.olog.data
 import android.database.ContentObserver
 import android.os.Handler
 import android.os.Looper
+import dev.olog.shared.CustomScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 internal class DataObserver(
     private val onUpdate: () -> Unit
-) : ContentObserver(Handler(Looper.getMainLooper())) {
+) : ContentObserver(Handler(Looper.getMainLooper())), CoroutineScope by CustomScope() {
 
     override fun onChange(selfChange: Boolean) {
         super.onChange(selfChange)
-        onUpdate()
+        launch { onUpdate() }
     }
 }
