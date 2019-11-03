1a1cb109b60e2dabde3255ed8deaff543d75b5d9
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-11-03 16:43:02 +0100

    restored data classes

--- core/src/main/java/dev/olog/core/entity/LastMetadata.kt
+++ core/src/main/java/dev/olog/core/entity/LastMetadata.kt
@@ -1,5 +1,7 @@
 package dev.olog.core.entity
 
+import android.provider.MediaStore
+
 data class LastMetadata(
     val title: String,
     val subtitle: String,
@@ -9,5 +11,13 @@ data class LastMetadata(
     fun isNotEmpty(): Boolean {
         return title.isNotBlank()
     }
+
+    val description: String
+        get() {
+            if (subtitle == MediaStore.UNKNOWN_STRING) {
+                return title
+            }
+            return "$title $subtitle"
+        }
 
 }
