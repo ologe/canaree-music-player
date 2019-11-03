ae267ab73b221f27cadbac124a33987a3fcb9411
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-11-03 16:25:07 +0100

    restored data classes

--- core/src/main/java/dev/olog/core/entity/track/Album.kt
+++ core/src/main/java/dev/olog/core/entity/track/Album.kt
@@ -3,54 +3,17 @@ package dev.olog.core.entity.track
 import dev.olog.core.MediaId
 import dev.olog.core.MediaIdCategory
 
-class Album(
-    @JvmField
+data class Album(
     val id: Long,
-    @JvmField
     val artistId: Long,
-    @JvmField
     val title: String,
-    @JvmField
     val artist: String,
-    @JvmField
     val albumArtist: String,
-    @JvmField
     val songs: Int,
-    @JvmField
     val hasSameNameAsFolder: Boolean,
-    @JvmField
     val isPodcast: Boolean
 ) {
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Album
-
-        if (id != other.id) return false
-        if (artistId != other.artistId) return false
-        if (title != other.title) return false
-        if (artist != other.artist) return false
-        if (albumArtist != other.albumArtist) return false
-        if (songs != other.songs) return false
-        if (hasSameNameAsFolder != other.hasSameNameAsFolder) return false
-        if (isPodcast != other.isPodcast) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id.hashCode()
-        result = 31 * result + artistId.hashCode()
-        result = 31 * result + title.hashCode()
-        result = 31 * result + artist.hashCode()
-        result = 31 * result + albumArtist.hashCode()
-        result = 31 * result + songs
-        result = 31 * result + hasSameNameAsFolder.hashCode()
-        result = 31 * result + isPodcast.hashCode()
-        return result
-    }
 
     fun getMediaId(): MediaId {
         val category = if (isPodcast) MediaIdCategory.PODCASTS_ALBUMS else MediaIdCategory.ALBUMS
--- core/src/main/java/dev/olog/core/entity/track/Genre.kt
+++ core/src/main/java/dev/olog/core/entity/track/Genre.kt
@@ -3,34 +3,11 @@ package dev.olog.core.entity.track
 import dev.olog.core.MediaId
 import dev.olog.core.MediaIdCategory
 
-class Genre(
-    @JvmField
+data class Genre(
     val id: Long,
-    @JvmField
     val name: String,
-    @JvmField
     val size: Int
 ) {
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Genre
-
-        if (id != other.id) return false
-        if (name != other.name) return false
-        if (size != other.size) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id.hashCode()
-        result = 31 * result + name.hashCode()
-        result = 31 * result + size
-        return result
-    }
 
     fun getMediaId(): MediaId {
         return MediaId.createCategoryValue(MediaIdCategory.GENRES, id.toString())
