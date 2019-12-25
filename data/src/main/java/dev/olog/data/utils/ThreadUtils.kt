8cac881fa3ee6a111df84cca00254f302bd6937b
Eugeniu Olog <eugeniu.olog@gmail.com>
2019-12-25 22:23:39 +0100

    cleaning

--- data/src/main/java/dev/olog/data/utils/ThreadUtils.kt
+++ data/src/main/java/dev/olog/data/utils/ThreadUtils.kt
@@ -8,18 +8,26 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.collect
 
+//private val isTestMode by lazy {
+//    try {
+//        Class.forName("org.junit.Test")
+//        true
+//    } catch (ignored: Throwable) {
+//        false
+//    }
+//}
+
 inline fun isMainThread() = Looper.myLooper() == Looper.getMainLooper()
 
-@Suppress("NOTHING_TO_INLINE")
-inline fun <T> Flow<T>.assertBackground(): Flow<T> {
+fun <T> Flow<T>.assertBackground(): Flow<T> {
     return channelFlow {
         assertBackgroundThread()
         collect { offer(it) }
     }
 }
 
-inline fun assertBackgroundThread() {
-    if (BuildConfig.DEBUG && isMainThread()) {
+fun assertBackgroundThread() {
+    if (/*!isTestMode &&*/ BuildConfig.DEBUG && isMainThread()) {
         throw AssertionError("not on worker thread, current=${Thread.currentThread()}")
     }
 }
