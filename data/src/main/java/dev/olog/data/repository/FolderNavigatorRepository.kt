42ab5f1e4507c369bc3b0b2ea41d654e427b15df
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-01-02 23:57:48 +0100

    cleaning

--- data/src/main/java/dev/olog/data/repository/FolderNavigatorRepository.kt
+++ data/src/main/java/dev/olog/data/repository/FolderNavigatorRepository.kt
@@ -1,26 +1,27 @@
 package dev.olog.data.repository
 
 import android.content.Context
-import android.database.ContentObserver
-import android.os.Handler
-import android.os.Looper
 import android.provider.MediaStore
 import dev.olog.core.dagger.ApplicationContext
 import dev.olog.core.entity.FileType
 import dev.olog.core.gateway.FolderNavigatorGateway
 import dev.olog.core.gateway.track.FolderGateway
 import dev.olog.core.prefs.BlacklistPreferences
+import dev.olog.core.schedulers.Schedulers
+import dev.olog.data.DataObserver
 import dev.olog.data.utils.assertBackground
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.channelFlow
 import java.io.File
+import java.util.*
 import javax.inject.Inject
 
 internal class FolderNavigatorRepository @Inject constructor(
     @ApplicationContext private val context: Context,
     private val blacklistGateway: BlacklistPreferences,
-    private val folderGateway: FolderGateway
+    private val folderGateway: FolderGateway,
+    private val schedulers: Schedulers
 ) : FolderNavigatorGateway {
 
     override fun observeFolderChildren(file: File): Flow<List<FileType>> {
@@ -28,7 +29,7 @@ internal class FolderNavigatorRepository @Inject constructor(
 
             offer(queryFileChildren(file))
 
-            val observer = ActionContentObserver { offer(queryFileChildren(file)) }
+            val observer = DataObserver(schedulers.io) { offer(queryFileChildren(file)) }
 
             context.contentResolver.registerContentObserver(
                 MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
@@ -56,20 +57,9 @@ internal class FolderNavigatorRepository @Inject constructor(
 
     private fun sortFolders(files: List<File>): List<FileType> {
         return files.asSequence()
-            .sortedBy { it.name.toLowerCase() }
+            .sortedBy { it.name.toLowerCase(Locale.ROOT) }
             .map { FileType.Folder(it.name, it.path) }
             .toList()
-    }
-
-}
-
-private class ActionContentObserver(
-    private val action: () -> Unit
-) : ContentObserver(Handler(Looper.getMainLooper())) {
-
-    override fun onChange(selfChange: Boolean) {
-        super.onChange(selfChange)
-        action()
     }
 
 }
