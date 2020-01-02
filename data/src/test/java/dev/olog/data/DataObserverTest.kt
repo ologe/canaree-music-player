a578f14c5ff265a2556e728a43287b20990e4160
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-01-02 22:42:48 +0100

    added data observer test

---
+++ data/src/test/java/dev/olog/data/DataObserverTest.kt
@@ -0,0 +1,26 @@
+package dev.olog.data
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import dev.olog.test.shared.MainCoroutineRule
+import dev.olog.test.shared.runBlocking
+import org.junit.Rule
+import org.junit.Test
+
+class DataObserverTest {
+
+    @get:Rule
+    val coroutineRule = MainCoroutineRule()
+
+    @Test
+    fun testInvoke() = coroutineRule.runBlocking {
+        coroutineRule.testDispatcher.asSchedulers()
+        val callback = mock<() -> Unit>()
+        val sut = DataObserver(coroutineRule.testDispatcher, callback)
+
+        sut.onChange(false)
+
+        verify(callback).invoke()
+    }
+
+}
