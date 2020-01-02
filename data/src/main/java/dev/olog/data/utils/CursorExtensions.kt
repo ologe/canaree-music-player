3c320f1c5360cbe6bfc0687a13cdcc15ef2adc3b
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-01-02 23:09:54 +0100

    refactor

--- data/src/main/java/dev/olog/data/utils/CursorExtensions.kt
+++ data/src/main/java/dev/olog/data/utils/CursorExtensions.kt
@@ -7,7 +7,7 @@ import android.database.Cursor
 internal inline fun Cursor.getInt(columnName: String): Int {
     try {
         return this.getInt(this.getColumnIndex(columnName))
-    } catch (ex: IllegalStateException) {
+    } catch (ex: Throwable) {
         throw IllegalStateException("invalid column $columnName", ex)
     }
 }
@@ -15,7 +15,7 @@ internal inline fun Cursor.getInt(columnName: String): Int {
 internal inline fun Cursor.getLong(columnName: String): Long {
     try {
         return this.getLong(this.getColumnIndex(columnName))
-    } catch (ex: IllegalStateException) {
+    } catch (ex: Throwable) {
         throw IllegalStateException("invalid column $columnName", ex)
     }
 }
@@ -23,32 +23,15 @@ internal inline fun Cursor.getLong(columnName: String): Long {
 internal inline fun Cursor.getString(columnName: String): String {
     try {
         return this.getString(this.getColumnIndex(columnName))
-    } catch (ex: IllegalStateException) {
+    } catch (ex: Throwable) {
         throw IllegalStateException("invalid column $columnName", ex)
     }
-}
-
-internal inline fun Cursor.getIntOrNull(columnName: String): Int? {
-    try {
-        return this.getInt(this.getColumnIndex(columnName))
-    } catch (ex: IllegalStateException) {
-        throw IllegalStateException("invalid column $columnName", ex)
-    }
-}
-
-internal inline fun Cursor.getLongOrNull(columnName: String): Long? {
-    try {
-        return this.getLong(this.getColumnIndex(columnName))
-    } catch (ex: IllegalStateException) {
-        throw IllegalStateException("invalid column $columnName", ex)
-    }
-
 }
 
 internal inline fun Cursor.getStringOrNull(columnName: String): String? {
     try {
         return this.getString(this.getColumnIndex(columnName))
-    } catch (ex: IllegalStateException) {
+    } catch (ex: Throwable) {
         throw IllegalStateException("invalid column $columnName", ex)
     }
 }
