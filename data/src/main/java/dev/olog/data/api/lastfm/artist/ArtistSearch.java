da78a7eb2dd837b7442bf49a771a7fd80775588f
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-01-06 16:11:54 +0100

    removed useless expose annotatoin

--- data/src/main/java/dev/olog/data/api/lastfm/artist/ArtistSearch.java
+++ data/src/main/java/dev/olog/data/api/lastfm/artist/ArtistSearch.java
@@ -1,18 +1,15 @@
 
 package dev.olog.data.api.lastfm.artist;
 
-import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class ArtistSearch {
 
     @SerializedName("results")
-    @Expose
     public Results results;
 
     public static class Results {
 
-        @Expose
         public Artistmatches artistmatches;
 
     }
--- data/src/main/java/dev/olog/data/api/lastfm/artist/Artistmatches.java
+++ data/src/main/java/dev/olog/data/api/lastfm/artist/Artistmatches.java
@@ -1,7 +1,6 @@
 
 package dev.olog.data.api.lastfm.artist;
 
-import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.ArrayList;
@@ -10,26 +9,20 @@ import java.util.List;
 public class Artistmatches {
 
     @SerializedName("artist")
-    @Expose
     public List<Artist> artist = new ArrayList<>();
 
     public static class Artist {
 
-        @Expose
         public String name;
 
-        @Expose
         public String mbid;
 
-        @Expose
         public List<Image> image = new ArrayList<>();
 
         public static class Image {
 
             @SerializedName("#text")
-            @Expose
             public String text;
-            @Expose
             public String size;
         }
 
