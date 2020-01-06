059e2196d50c6ca814bd9dbe8c9db4adeead7591
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-01-06 16:48:39 +0100

    refactor

--- data/src/main/java/dev/olog/data/api/LastFmService.kt
+++ data/src/main/java/dev/olog/data/api/LastFmService.kt
@@ -2,54 +2,51 @@ package dev.olog.data.api
 
 import androidx.annotation.IntRange
 import dev.olog.data.BuildConfig
-import dev.olog.data.model.lastfm.AlbumInfo
-import dev.olog.data.model.lastfm.AlbumSearch
-import dev.olog.data.model.lastfm.ArtistInfo
-import dev.olog.data.model.lastfm.ArtistSearch
-import dev.olog.data.model.lastfm.TrackInfo
-import dev.olog.data.model.lastfm.TrackSearch
+import dev.olog.data.model.lastfm.*
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-private const val MIN_SEARCH_PAGES = 1L
-private const val MAX_SEARCH_PAGES = 5L
-private const val DEFAULT_SEARCH_PAGES = MAX_SEARCH_PAGES
-
-private const val DEFAULT_AUTO_CORRECT = 1L
-
-private const val BASE_URL = "?api_key=${BuildConfig.LAST_FM_KEY}&format=json"
-
 interface LastFmService {
+
+    companion object {
+        private const val MIN_SEARCH_PAGES = 1L
+        private const val MAX_SEARCH_PAGES = 5L
+        private const val DEFAULT_SEARCH_PAGES = MAX_SEARCH_PAGES
+
+        private const val DEFAULT_AUTO_CORRECT = 1L
+
+        private const val BASE_URL = "?api_key=${BuildConfig.LAST_FM_KEY}&format=json"
+    }
 
     @GET("$BASE_URL&method=track.getInfo")
     suspend fun getTrackInfoAsync(
-            @Query("track", encoded = true) track: String,
-            @Query("artist", encoded = true) artist: String,
-            @IntRange(from = 0, to = 1) @Query("autocorrect") autocorrect: Long = DEFAULT_AUTO_CORRECT
-    ) : Response<TrackInfo>
+        @Query("track", encoded = true) track: String,
+        @Query("artist", encoded = true) artist: String,
+        @IntRange(from = 0, to = 1) @Query("autocorrect") autocorrect: Long = DEFAULT_AUTO_CORRECT
+    ): Response<TrackInfo>
 
     @GET("$BASE_URL&method=track.search")
     suspend fun searchTrackAsync(
-            @Query("track", encoded = true) track: String,
-            @Query("artist", encoded = true) artist: String = "",
-            @IntRange(from = MIN_SEARCH_PAGES, to = MAX_SEARCH_PAGES)
-            @Query("limit") limit: Long = DEFAULT_SEARCH_PAGES
+        @Query("track", encoded = true) track: String,
+        @Query("artist", encoded = true) artist: String = "",
+        @IntRange(from = MIN_SEARCH_PAGES, to = MAX_SEARCH_PAGES)
+        @Query("limit") limit: Long = DEFAULT_SEARCH_PAGES
     ): Response<TrackSearch>
 
     @GET("$BASE_URL&method=artist.getinfo")
     suspend fun getArtistInfoAsync(
         @Query("artist", encoded = true) artist: String,
         @IntRange(from = 0, to = 1)
-            @Query("autocorrect") autocorrect: Long = DEFAULT_AUTO_CORRECT,
+        @Query("autocorrect") autocorrect: Long = DEFAULT_AUTO_CORRECT,
         @Query("lang") language: String = "en"
     ): Response<ArtistInfo>
 
     @GET("$BASE_URL&method=artist.search")
     suspend fun searchArtistAsync(
-            @Query("artist", encoded = true) artist: String,
-            @IntRange(from = MIN_SEARCH_PAGES, to = MAX_SEARCH_PAGES)
-            @Query("limit") limit: Long = DEFAULT_SEARCH_PAGES
+        @Query("artist", encoded = true) artist: String,
+        @IntRange(from = MIN_SEARCH_PAGES, to = MAX_SEARCH_PAGES)
+        @Query("limit") limit: Long = DEFAULT_SEARCH_PAGES
     ): Response<ArtistSearch>
 
     @GET("$BASE_URL&method=album.getinfo")
@@ -57,15 +54,15 @@ interface LastFmService {
         @Query("album", encoded = true) album: String,
         @Query("artist", encoded = true) artist: String,
         @IntRange(from = 0, to = 1)
-            @Query("autocorrect") autocorrect: Long= DEFAULT_AUTO_CORRECT,
+        @Query("autocorrect") autocorrect: Long = DEFAULT_AUTO_CORRECT,
         @Query("lang") language: String = "en"
     ): Response<AlbumInfo>
 
     @GET("$BASE_URL&method=album.search")
     suspend fun searchAlbumAsync(
-            @Query("album", encoded = true) album: String,
-            @IntRange(from = MIN_SEARCH_PAGES, to = MAX_SEARCH_PAGES)
-            @Query("limit") limit: Long = DEFAULT_SEARCH_PAGES
+        @Query("album", encoded = true) album: String,
+        @IntRange(from = MIN_SEARCH_PAGES, to = MAX_SEARCH_PAGES)
+        @Query("limit") limit: Long = DEFAULT_SEARCH_PAGES
     ): Response<AlbumSearch>
 
 }
