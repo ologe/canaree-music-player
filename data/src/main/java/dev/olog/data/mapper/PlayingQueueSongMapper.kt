d4ad9a766fbeafc1bcdb6fe03e6289c559522092
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-01-06 19:48:21 +0100

    cleaning

---
+++ data/src/main/java/dev/olog/data/mapper/PlayingQueueSongMapper.kt
@@ -0,0 +1,11 @@
+package dev.olog.data.mapper
+
+import dev.olog.core.entity.PlayingQueueSong
+import dev.olog.core.entity.track.Song
+
+internal fun Song.toPlayingQueueSong(progressive: Int): PlayingQueueSong {
+    return PlayingQueueSong(
+        song = this.copy(idInPlaylist = progressive),
+        mediaId = getMediaId()
+    )
+}
