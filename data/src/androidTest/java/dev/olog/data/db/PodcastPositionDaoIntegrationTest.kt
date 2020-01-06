a64478c40c0486488af399517c9886bddbe89abb
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-01-06 21:21:17 +0100

    added `PodcastPositionDao` integration test

---
+++ data/src/androidTest/java/dev/olog/data/db/PodcastPositionDaoIntegrationTest.kt
@@ -0,0 +1,58 @@
+package dev.olog.data.db
+
+import android.app.Application
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import dev.olog.data.model.db.PodcastPositionEntity
+import dev.olog.test.shared.MainCoroutineRule
+import dev.olog.test.shared.runBlocking
+import kotlinx.coroutines.asExecutor
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.io.IOException
+
+class PodcastPositionDaoIntegrationTest {
+
+    @get:Rule
+    val coroutinesRule = MainCoroutineRule()
+
+    private lateinit var db: AppDatabase
+    private lateinit var sut: PodcastPositionDao
+
+    @Before
+    fun setup() {
+        val context = ApplicationProvider.getApplicationContext<Application>()
+        db = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java)
+            .setQueryExecutor(coroutinesRule.testDispatcher.asExecutor())
+            .build()
+        sut = db.podcastPositionDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun teardown() {
+        db.close()
+    }
+
+    @Test
+    fun testGetAndSet() = coroutinesRule.runBlocking {
+        // given
+        val id = 1L
+        val positionEntity = PodcastPositionEntity(id, 15000)
+        assertNull("should be ull", sut.getPosition(id))
+
+        // when
+        sut.setPosition(positionEntity)
+
+        // then
+        assertEquals(
+            positionEntity.position,
+            sut.getPosition(id)
+        )
+    }
+
+}
