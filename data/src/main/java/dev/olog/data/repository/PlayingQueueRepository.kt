27b0b30f6be6d22d008604e3831eaa71a3f4bae4
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-01-06 19:50:22 +0100

    cleaning

--- data/src/main/java/dev/olog/data/repository/PlayingQueueRepository.kt
+++ data/src/main/java/dev/olog/data/repository/PlayingQueueRepository.kt
@@ -1,12 +1,12 @@
 package dev.olog.data.repository
 
 import dev.olog.core.entity.PlayingQueueSong
-import dev.olog.core.entity.track.Song
 import dev.olog.core.gateway.PlayingQueueGateway
 import dev.olog.core.gateway.podcast.PodcastGateway
 import dev.olog.core.gateway.track.SongGateway
 import dev.olog.core.interactor.UpdatePlayingQueueUseCaseRequest
 import dev.olog.data.db.PlayingQueueDao
+import dev.olog.data.mapper.toPlayingQueueSong
 import dev.olog.data.utils.assertBackground
 import dev.olog.data.utils.assertBackgroundThread
 import kotlinx.coroutines.flow.Flow
@@ -21,7 +21,6 @@ internal class PlayingQueueRepository @Inject constructor(
 
     override fun getAll(): List<PlayingQueueSong> {
         try {
-//            assertBackgroundThread()
             val playingQueue =
                 playingQueueDao.getAllAsSongs(songGateway.getAll(), podcastGateway.getAll())
             if (playingQueue.isNotEmpty()) {
@@ -43,13 +42,6 @@ internal class PlayingQueueRepository @Inject constructor(
     override fun update(list: List<UpdatePlayingQueueUseCaseRequest>) {
         assertBackgroundThread()
         playingQueueDao.insert(list)
-    }
-
-    private fun Song.toPlayingQueueSong(progressive: Int): PlayingQueueSong {
-        return PlayingQueueSong(
-            this.copy(idInPlaylist = progressive),
-            getMediaId()
-        )
     }
 
 }
