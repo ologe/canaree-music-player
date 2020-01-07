5f75e18e8cbe2f65d38eff39033aeceb4070156e
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-01-07 21:57:52 +0100

    typo fix

--- core/src/main/java/dev/olog/core/interactor/search/DeleteRecentSearchUseCase.kt
+++ core/src/main/java/dev/olog/core/interactor/search/DeleteRecentSearchUseCase.kt
@@ -23,7 +23,7 @@ class DeleteRecentSearchUseCase @Inject constructor(
             mediaId.isPodcastPlaylist -> recentSearchesGateway.deletePodcastPlaylist(id)
             mediaId.isPodcastAlbum -> recentSearchesGateway.deletePodcastAlbum(id)
             mediaId.isPodcastArtist -> recentSearchesGateway.deletePodcastArtist(id)
-            else -> throw IllegalArgumentException("invalid category ${mediaId.resolveId}")
+            else -> throw IllegalArgumentException("invalid category ${mediaId.category}")
         }
     }
 }
