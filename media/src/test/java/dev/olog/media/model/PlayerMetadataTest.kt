6df9c3845517aa3c1e540b0236fee282286ea965
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-01-18 21:40:25 +0100

    added robolectric

---
+++ media/src/test/java/dev/olog/media/model/PlayerMetadataTest.kt
@@ -0,0 +1,35 @@
+package dev.olog.media.model
+
+import android.support.v4.media.MediaMetadataCompat
+import dev.olog.core.MediaId
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [28])
+class PlayerMetadataTest {
+
+    @Test
+    fun testMediaId() {
+        // given
+        val mediaId = MediaId.shuffleId
+
+        val metadata = MediaMetadataCompat.Builder()
+            .putString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID, mediaId.toString())
+            .build()
+
+        val sut = PlayerMetadata(metadata)
+
+        // when
+        val actual = sut.mediaId
+
+        assertEquals(
+            mediaId,
+            actual
+        )
+    }
+
+}
