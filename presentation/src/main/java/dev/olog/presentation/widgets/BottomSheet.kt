79b7370979d9f4d7a75056d2ec43bc5b76fd89a8
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-03-02 22:59:54 +0100

    made `player_layout_mini` similar to `mini_player_floating`

--- presentation/src/main/java/dev/olog/presentation/widgets/BottomSheet.kt
+++ presentation/src/main/java/dev/olog/presentation/widgets/BottomSheet.kt
@@ -2,7 +2,10 @@ package dev.olog.presentation.widgets
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.widget.FrameLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import dev.olog.presentation.interfaces.HasSlidingPanel
 import dev.olog.shared.android.extensions.colorBackground
 import dev.olog.shared.android.extensions.dipf
 import dev.olog.shared.android.theme.BottomSheetType
@@ -20,14 +23,44 @@ class BottomSheet(
         }
     }
 
+    private val color = context.colorBackground()
+
     private fun setupDefault() {
         elevation = context.dipf(8)
+        // for some reason after inlining above `color` shadow is not drawn
         setBackgroundColor(context.colorBackground())
     }
 
     private fun setupElevated() {
         elevation = 0f
         background = null
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        val themeManager = context.themeManager
+        if (themeManager.playerAppearance.isMini && themeManager.bottomSheetType == BottomSheetType.DEFAULT) {
+            (context as HasSlidingPanel).getSlidingPanel().addBottomSheetCallback(listener)
+        }
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        val themeManager = context.themeManager
+        if (themeManager.playerAppearance.isMini && themeManager.bottomSheetType == BottomSheetType.DEFAULT) {
+            (context as HasSlidingPanel).getSlidingPanel().removeBottomSheetCallback(listener)
+        }
+    }
+
+    private val listener = object : BottomSheetBehavior.BottomSheetCallback() {
+        override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            val alpha = 1f - slideOffset
+            background?.alpha = (255f * alpha).toInt()
+        }
+
+        override fun onStateChanged(bottomSheet: View, newState: Int) {
+
+        }
     }
 
 }
