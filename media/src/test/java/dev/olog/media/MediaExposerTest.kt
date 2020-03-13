031da05b05190223178a675d36fa0c8d7b77acc2
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-03-14 00:14:39 +0100

    MediaExposerTest still WIP

--- media/src/test/java/dev/olog/media/MediaExposerTest.kt
+++ media/src/test/java/dev/olog/media/MediaExposerTest.kt
@@ -6,9 +6,17 @@ import dev.olog.media.connection.MusicServiceConnectionState
 import dev.olog.media.connection.OnConnectionChanged
 import dev.olog.media.model.*
 import dev.olog.test.shared.MainCoroutineRule
+import dev.olog.test.shared.runBlockingTest
 import kotlinx.coroutines.channels.ConflatedBroadcastChannel
 import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
+// TODO made testable
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [28])
 class MediaExposerTest {
 
     @get:Rule
@@ -32,13 +40,29 @@ class MediaExposerTest {
         shuffleModePublisher,
         queuePublisher
     )
-    private val sut = MediaExposer(
-        context,
-        onConnectionChanged,
-        coroutineRule.testDispatcher.asSchedulers(),
-        config
-    )
+    private lateinit var sut: MediaExposer
 
+    @Test
+    fun `test connect, no permission`() = coroutineRule.runBlockingTest {
+        sut = buildSut(hasPermissionStorage = false)
 
+        sut.connect()
+    }
+
+    @Test
+    fun `test connect, success`() = coroutineRule.runBlockingTest {
+        sut = buildSut()
+
+        sut.connect()
+    }
+
+    private fun buildSut(hasPermissionStorage: Boolean = true): MediaExposer {
+        return MediaExposer(
+            context,
+            onConnectionChanged,
+            coroutineRule.testDispatcher.asSchedulers(),
+            config
+        ) { hasPermissionStorage }
+    }
 
 }
