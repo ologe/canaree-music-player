f63a4edd502a3f932b4a3197ea87436f34ce29ef
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-03-14 01:37:36 +0100

    cleaning

--- offline-lyrics/src/main/java/dev/olog/offlinelyrics/OfflineLyricsLine.kt
+++ offline-lyrics/src/main/java/dev/olog/offlinelyrics/OfflineLyricsLine.kt
@@ -1,10 +1,10 @@
 package dev.olog.offlinelyrics
 
-internal data class Lyrics(
+data class Lyrics(
     val lines: List<OfflineLyricsLine>
 )
 
-internal data class OfflineLyricsLine(
+data class OfflineLyricsLine(
     val value: String,
     val time: Long
 )
