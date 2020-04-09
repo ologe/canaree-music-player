9439886419368e85d69bfff418e6832fa0d1238c
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-04-09 23:50:35 +0200

    added library entrypoint

--- feature-library/src/main/java/dev/olog/feature/library/dagger/FeatureLibraryDagger.kt
+++ feature-library/src/main/java/dev/olog/feature/library/dagger/FeatureLibraryDagger.kt
@@ -1,11 +1,16 @@
 package dev.olog.feature.library.dagger
 
+import androidx.fragment.app.Fragment
 import dagger.Module
+import dagger.Provides
 import dagger.android.ContributesAndroidInjector
+import dagger.multibindings.IntoMap
 import dev.olog.feature.library.folder.tree.FolderTreeFragment
 import dev.olog.feature.library.library.LibraryFragment
 import dev.olog.feature.library.tab.TabFragment
 import dev.olog.feature.presentation.base.dagger.ScreenScope
+import dev.olog.navigation.dagger.FragmentScreenKey
+import dev.olog.navigation.screens.FragmentScreen
 
 class FeatureLibraryDagger {
 
@@ -23,6 +28,24 @@ class FeatureLibraryDagger {
         @ContributesAndroidInjector(modules = [FolderTreeFragmentModule::class])
         @ScreenScope
         internal abstract fun provideFolderTreeFragment(): FolderTreeFragment
+
+        companion object {
+
+            @Provides
+            @IntoMap
+            @FragmentScreenKey(FragmentScreen.LIBRARY_TRACKS)
+            internal fun provideLibraryTracksFragment(): Fragment {
+                return LibraryFragment.newInstance(false)
+            }
+
+            @Provides
+            @IntoMap
+            @FragmentScreenKey(FragmentScreen.LIBRARY_PODCAST)
+            internal fun provideLibraryPodcastFragment(): Fragment {
+                return LibraryFragment.newInstance(true)
+            }
+
+        }
 
     }
 
