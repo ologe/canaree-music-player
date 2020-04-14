7b93b2c7fa18eb0ffdb85f7a794c973134b55b84
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-04-14 18:05:50 +0200

    deleted layout manager factory

--- features/library/src/main/java/dev/olog/feature/library/tab/layout/manager/SongSpanSizeLookup.kt
+++
@@ -1,8 +0,0 @@
-package dev.olog.feature.library.tab.layout.manager
-
-internal class SongSpanSizeLookup(
-    requestedSpanSize: Int
-) : AbsSpanSizeLookup(requestedSpanSize) {
-
-    override fun getSpanSize(position: Int): Int = getSpanCount() / requestedSpanSize
-}
