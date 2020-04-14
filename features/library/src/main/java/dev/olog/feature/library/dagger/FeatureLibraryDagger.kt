4c2ace575c66ffb666680974c7f0f9718b67febe
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-04-14 17:35:34 +0200

    updated dagger appmodule

--- features/library/src/main/java/dev/olog/feature/library/dagger/FeatureLibraryDagger.kt
+++ features/library/src/main/java/dev/olog/feature/library/dagger/FeatureLibraryDagger.kt
@@ -6,9 +6,16 @@ import dagger.Provides
 import dagger.android.ContributesAndroidInjector
 import dagger.multibindings.IntoMap
 import dev.olog.core.dagger.FeatureScope
-import dev.olog.feature.library.folder.tree.FolderTreeFragment
-import dev.olog.feature.library.library.LibraryFragment
-import dev.olog.feature.library.tab.TabFragment
+import dev.olog.feature.library.album.AlbumsFragment
+import dev.olog.feature.library.artist.ArtistsFragment
+import dev.olog.feature.library.dagger.module.*
+import dev.olog.feature.library.folder.FoldersFragment
+import dev.olog.feature.library.folder.normal.FoldersNormalFragment
+import dev.olog.feature.library.folder.tree.FoldersTreeFragment
+import dev.olog.feature.library.genre.GenresFragment
+import dev.olog.feature.library.home.HomeFragment
+import dev.olog.feature.library.playlists.PlaylistsFragment
+import dev.olog.feature.library.tracks.TracksFragment
 import dev.olog.navigation.dagger.FragmentScreenKey
 import dev.olog.navigation.screens.FragmentScreen
 
@@ -17,32 +24,93 @@ class FeatureLibraryDagger {
     @Module
     abstract class AppModule {
 
+        @ContributesAndroidInjector(modules = [HomeFragmentModule::class])
+        @FeatureScope
+        internal abstract fun provideHomeFragment(): HomeFragment
+
+        @ContributesAndroidInjector(modules = [TracksFragmentModule::class])
+        @FeatureScope
+        internal abstract fun provideTabAltFragment(): TracksFragment
+
+        @ContributesAndroidInjector(modules = [PlaylistsFragmentModule::class])
+        @FeatureScope
+        internal abstract fun providePlaylistFragment(): PlaylistsFragment
+
+        @ContributesAndroidInjector(modules = [AlbumsFragmentModule::class])
+        @FeatureScope
+        internal abstract fun provideAlbumsFragment(): AlbumsFragment
+
+        @ContributesAndroidInjector(modules = [ArtistsFragmentModule::class])
+        @FeatureScope
+        internal abstract fun provideArtistsFragment(): ArtistsFragment
+
+        // genres
+        @ContributesAndroidInjector(modules = [GenresFragmentModule::class])
+        @FeatureScope
+        internal abstract fun provideGenreFragment(): GenresFragment
+
+        // folders
         @ContributesAndroidInjector
         @FeatureScope
-        internal abstract fun provideCategoriesFragment(): LibraryFragment
+        internal abstract fun provideFolderFragment(): FoldersFragment
 
-        @ContributesAndroidInjector(modules = [TabFragmentModule::class])
+        @ContributesAndroidInjector(modules = [FoldersNormalFragmentModule::class])
         @FeatureScope
-        internal abstract fun provideTabFragment(): TabFragment
+        internal abstract fun provideFolderNormalFragment(): FoldersNormalFragment
 
-        @ContributesAndroidInjector(modules = [FolderTreeFragmentModule::class])
+        @ContributesAndroidInjector(modules = [FoldersTreeFragmentModule::class])
         @FeatureScope
-        internal abstract fun provideFolderTreeFragment(): FolderTreeFragment
+        internal abstract fun provideFolderTreeFragment(): FoldersTreeFragment
 
         companion object {
 
             @Provides
             @IntoMap
-            @FragmentScreenKey(FragmentScreen.LIBRARY_TRACKS)
-            internal fun provideLibraryTracksFragment(): Fragment {
-                return LibraryFragment.newInstance(false)
+            @FragmentScreenKey(FragmentScreen.HOME)
+            internal fun provideHomeFragment(): Fragment {
+                return HomeFragment()
             }
 
             @Provides
             @IntoMap
-            @FragmentScreenKey(FragmentScreen.LIBRARY_PODCAST)
-            internal fun provideLibraryPodcastFragment(): Fragment {
-                return LibraryFragment.newInstance(true)
+            @FragmentScreenKey(FragmentScreen.TRACKS)
+            internal fun provideTracksFragment(): Fragment {
+                return TracksFragment()
+            }
+
+            @Provides
+            @IntoMap
+            @FragmentScreenKey(FragmentScreen.PLAYLISTS)
+            internal fun providePlaylistFragment(): Fragment {
+                return PlaylistsFragment()
+            }
+
+            @Provides
+            @IntoMap
+            @FragmentScreenKey(FragmentScreen.ALBUMS)
+            internal fun provideAlbumsFragment(): Fragment {
+                return AlbumsFragment()
+            }
+
+            @Provides
+            @IntoMap
+            @FragmentScreenKey(FragmentScreen.ARTISTS)
+            internal fun provideArtistFragment(): Fragment {
+                return ArtistsFragment()
+            }
+
+            @Provides
+            @IntoMap
+            @FragmentScreenKey(FragmentScreen.GENRES)
+            internal fun provideGenreFragment(): Fragment {
+                return GenresFragment()
+            }
+
+            @Provides
+            @IntoMap
+            @FragmentScreenKey(FragmentScreen.FOLDERS)
+            internal fun provideFolderFragment(): Fragment {
+                return FoldersFragment()
             }
 
         }
