ffc1da3a899976debcbdb2c7aa08fe38ae954d73
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-11-14 00:15:37 +0100

    migrated :analytics to hilt

--- analytics/src/main/java/dev/olog/analytics/AnalyticsModule.kt
+++ analytics/src/main/java/dev/olog/analytics/AnalyticsModule.kt
@@ -1,23 +1,31 @@
 package dev.olog.analytics
 
-import android.content.Context
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
+import dagger.Reusable
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
 import dev.olog.analytics.tracker.FirebaseTracker
-import dev.olog.core.dagger.ApplicationContext
-import javax.inject.Singleton
 
 @Module
-class AnalyticsModule {
+@InstallIn(ApplicationComponent::class)
+internal abstract class AnalyticsModule {
 
-    @Provides
-    internal fun provideFirebaseAnalytics(@ApplicationContext context: Context): FirebaseAnalytics {
-        return FirebaseAnalytics.getInstance(context)
+    @Binds
+    @Reusable
+    internal abstract fun provideTrackerFacade(impl: FirebaseTracker): TrackerFacade
+
+    companion object {
+
+        @Provides
+        internal fun provideFirebaseAnalytics(): FirebaseAnalytics {
+            return Firebase.analytics
+        }
+
     }
-
-    @Provides
-    @Singleton
-    internal fun provideTrackerFacade(impl: FirebaseTracker): TrackerFacade = impl
 
 }
