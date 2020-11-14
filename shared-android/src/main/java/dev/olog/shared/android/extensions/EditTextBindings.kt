590b02dce4fe7eeee395a10a48c770565cf09ada
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-11-14 19:47:13 +0100

    emit initial text as first value

--- shared-android/src/main/java/dev/olog/shared/android/extensions/EditTextBindings.kt
+++ shared-android/src/main/java/dev/olog/shared/android/extensions/EditTextBindings.kt
@@ -11,22 +11,17 @@ import kotlinx.coroutines.flow.onCompletion
 
 fun EditText.afterTextChange(): Flow<String> {
     assertMainThread()
-    val channel = ConflatedBroadcastChannel<String>()
+    val channel = ConflatedBroadcastChannel(text.toString())
 
     val watcher = object : TextWatcher {
-        override fun afterTextChanged(s: Editable?) {
+        override fun afterTextChanged(s: Editable) {
             if (!channel.isClosedForSend) {
-                channel.offer(s!!.toString())
+                channel.offer(s.toString())
             }
         }
 
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-        }
-
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-        }
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
     }
 
     addTextChangedListener(watcher)
