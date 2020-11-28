885f02f7631a36b266c971cc26c55c75d706db3e
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-11-28 20:37:22 +0100

    update BaseMusicService lifecycle

--- service-music/src/main/java/dev/olog/service/music/BaseMusicService.kt
+++ service-music/src/main/java/dev/olog/service/music/BaseMusicService.kt
@@ -2,6 +2,7 @@ package dev.olog.service.music
 
 import android.app.Service
 import android.content.Intent
+import android.os.IBinder
 import android.provider.MediaStore
 import android.util.Log
 import androidx.annotation.CallSuper
@@ -34,7 +35,26 @@ abstract class BaseMusicService : MediaBrowserServiceCompat(),
     private var serviceStarted = false
 
     @CallSuper
+    override fun onCreate() {
+        dispatcher.onServicePreSuperOnCreate()
+        super.onCreate()
+    }
+
+    @CallSuper
+    override fun onStart(intent: Intent?, startId: Int) {
+        dispatcher.onServicePreSuperOnStart()
+        super.onStart(intent, startId)
+    }
+
+    @CallSuper
+    override fun onBind(intent: Intent?): IBinder? {
+        dispatcher.onServicePreSuperOnBind()
+        return super.onBind(intent)
+    }
+
+    @CallSuper
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
         serviceStarted = true
 
         handleIntent(intent)
