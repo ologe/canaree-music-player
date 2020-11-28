385fb45c2472c5c560cce43c08e6cdff65d67ddf
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-11-28 14:47:17 +0100

    use function reference instead of creating a new lambda

--- shared/src/main/java/dev/olog/shared/FlowExtensions.kt
+++ shared/src/main/java/dev/olog/shared/FlowExtensions.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.map
 
 inline fun <T, R> Flow<List<T>>.mapListItem(crossinline mapper: (T) -> R): Flow<List<R>> {
-    return this.map { it.map { mapper(it) } }
+    return this.map { it.map(mapper) }
 }
 
 inline val <T> MutableSharedFlow<T>.value: T
