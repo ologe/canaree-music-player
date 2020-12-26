f7ccda0213e5234af9551a3f1c05f7694a21904c
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-12-26 17:39:59 +0100

    added fromPlatform helper

--- service-music/src/main/java/dev/olog/service/music/focus/FocusState.kt
+++ service-music/src/main/java/dev/olog/service/music/focus/FocusState.kt
@@ -1,6 +1,24 @@
 package dev.olog.service.music.focus
 
+import android.media.AudioManager
+
 internal enum class FocusState {
-    NONE, PLAY_WHEN_READY, DELAYED, GAIN;
+    NONE,
+    PLAY_WHEN_READY,
+    DELAYED,
+    GAIN;
+
+    companion object {
+
+        fun fromPlatform(focus: Int): FocusState {
+            return when (focus) {
+                AudioManager.AUDIOFOCUS_REQUEST_GRANTED -> GAIN
+                AudioManager.AUDIOFOCUS_REQUEST_DELAYED -> DELAYED
+                AudioManager.AUDIOFOCUS_REQUEST_FAILED -> NONE
+                else -> error("audio focus response not handle with code $focus")
+            }
+        }
+
+    }
 
 }
