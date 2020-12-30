ae166f978c5e7ed0cc169c7b2b1650688b4718b9
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-12-30 22:01:36 +0100

    cleaning

--- intents/src/main/java/dev/olog/intents/AppConstants.kt
+++ intents/src/main/java/dev/olog/intents/AppConstants.kt
@@ -5,9 +5,6 @@ import kotlin.time.milliseconds
 
 object AppConstants {
 
-    private const val TAG = "AppConstants"
-    const val ACTION_CONTENT_VIEW = "$TAG.action.content.view"
-
     val PROGRESS_BAR_INTERVAL = 50L.milliseconds
 
     const val UNKNOWN = MediaStore.UNKNOWN_STRING
