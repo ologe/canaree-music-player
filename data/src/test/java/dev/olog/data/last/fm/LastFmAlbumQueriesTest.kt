9712602ba9651e459cca6ef5e2d68ca4bd7aab09
Eugeniu Olog <eugeniu.olog@gmail.com>
2021-12-11 23:50:36 +0100

    - added LastFmQueries + tests

---
+++ data/src/test/java/dev/olog/data/last/fm/LastFmAlbumQueriesTest.kt
@@ -0,0 +1,72 @@
+package dev.olog.data.last.fm
+
+import dev.olog.data.TestDatabase
+import dev.olog.data.utils.DateFactory
+import org.junit.Assert
+import org.junit.Test
+import java.time.LocalDate
+import java.time.ZoneId
+import java.util.*
+
+class LastFmAlbumQueriesTest {
+
+    private val db = TestDatabase()
+    private val queries = db.lastFmAlbumQueriesQueries
+
+    @Test
+    fun `less than last month should be still valid`() {
+        Assert.assertEquals(null, queries.selectById(1).executeAsOneOrNull())
+
+        val instant = LocalDate.now()
+            .minusMonths(1).plusDays(1)
+            .atStartOfDay()
+            .atZone(ZoneId.systemDefault())
+            .toInstant()
+
+        val item = Last_fm_album(
+            id = 1,
+            title = "title",
+            artist = "artist",
+            image_url = "image",
+            added = DateFactory.millisToFormattedDate(Date.from(instant).time),
+            mbid = "mbid",
+            wiki = "wiki"
+        )
+
+        // insert
+        queries.insert(item)
+
+        Assert.assertEquals(item, queries.selectById(1).executeAsOne())
+
+        // delete
+        queries.delete(1)
+        Assert.assertEquals(null, queries.selectById(1).executeAsOneOrNull())
+    }
+
+    @Test
+    fun `more than last month should be invalid`() {
+        Assert.assertEquals(null, queries.selectById(1).executeAsOneOrNull())
+
+        val instant = LocalDate.now()
+            .minusMonths(1).minusDays(1)
+            .atStartOfDay()
+            .atZone(ZoneId.systemDefault())
+            .toInstant()
+
+        val item = Last_fm_album(
+            id = 1,
+            title = "title",
+            artist = "artist",
+            image_url = "image",
+            added = DateFactory.millisToFormattedDate(Date.from(instant).time),
+            mbid = "mbid",
+            wiki = "wiki"
+        )
+
+        // insert
+        queries.insert(item)
+
+        Assert.assertEquals(null, queries.selectById(1).executeAsOneOrNull())
+    }
+
+}
---
+++ data/src/test/java/dev/olog/data/last/fm/LastFmTrackQueriesTest.kt
@@ -0,0 +1,76 @@
+package dev.olog.data.last.fm
+
+import dev.olog.data.TestDatabase
+import dev.olog.data.utils.DateFactory
+import org.junit.Assert
+import org.junit.Test
+import java.time.LocalDate
+import java.time.ZoneId
+import java.util.*
+
+class LastFmTrackQueriesTest {
+
+    private val db = TestDatabase()
+    private val queries = db.lastFmTrackQueriesQueries
+
+    @Test
+    fun `less than last month should be still valid`() {
+        Assert.assertEquals(null, queries.selectById(1).executeAsOneOrNull())
+
+        val instant = LocalDate.now()
+            .minusMonths(1).plusDays(1)
+            .atStartOfDay()
+            .atZone(ZoneId.systemDefault())
+            .toInstant()
+
+        val item = Last_fm_track(
+            id = 1,
+            title = "title",
+            artist = "artist",
+            album = "album",
+            image_url = "image_url",
+            added = DateFactory.millisToFormattedDate(Date.from(instant).time),
+            mbid = "mbid",
+            artist_mbid = "artist_mbid",
+            album_mbid = "album_mbid",
+        )
+
+        // insert
+        queries.insert(item)
+
+        Assert.assertEquals(item, queries.selectById(1).executeAsOne())
+
+        // delete
+        queries.delete(1)
+        Assert.assertEquals(null, queries.selectById(1).executeAsOneOrNull())
+    }
+
+    @Test
+    fun `more than last month should be invalid`() {
+        Assert.assertEquals(null, queries.selectById(1).executeAsOneOrNull())
+
+        val instant = LocalDate.now()
+            .minusMonths(1).minusDays(1)
+            .atStartOfDay()
+            .atZone(ZoneId.systemDefault())
+            .toInstant()
+
+        val item = Last_fm_track(
+            id = 1,
+            title = "title",
+            artist = "artist",
+            album = "album",
+            image_url = "image_url",
+            added = DateFactory.millisToFormattedDate(Date.from(instant).time),
+            mbid = "mbid",
+            artist_mbid = "artist_mbid",
+            album_mbid = "album_mbid",
+        )
+
+        // insert
+        queries.insert(item)
+
+        Assert.assertEquals(null, queries.selectById(1).executeAsOneOrNull())
+    }
+
+}
