3e0d84880f857089ce5d27b53681ff9e0ac7b07d
Eugeniu Olog <eugeniu.olog@gmail.com>
2021-12-11 15:51:23 +0100

    - added podcast collection queries + tests

---
+++ data/src/androidTest/java/dev/olog/data/collection/SortedPodcastCollectionEpisodesQueriesTest.kt
@@ -0,0 +1,201 @@
+package dev.olog.data.collection
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import dev.olog.core.entity.sort.CollectionDetailSort
+import dev.olog.core.entity.sort.Sort
+import dev.olog.core.entity.sort.SortDirection
+import dev.olog.data.AndroidIndexedPlayables
+import dev.olog.data.AndroidTestDatabase
+import dev.olog.data.IndexedPodcastEpisodes
+import dev.olog.data.insertGroup
+import dev.olog.data.sort.SortDao
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+internal class SortedPodcastCollectionEpisodesQueriesTest {
+
+    private val db = AndroidTestDatabase()
+    private val indexedQueries = db.indexedPlayablesQueries
+    private val blacklistQueries = db.blacklistQueries
+    private val sortQueries = SortDao(db.sortQueries)
+    private val queries = db.podcastCollectionQueries
+
+    @Before
+    fun setup() {
+        blacklistQueries.insert("yes")
+        // item to be filtered, blacklisted and podcast
+        indexedQueries.insert(AndroidIndexedPlayables(id = 1000, is_podcast = true, directory = "yes"))
+        indexedQueries.insert(AndroidIndexedPlayables(id = 1001, is_podcast = false, directory = "no"))
+        indexedQueries.insert(AndroidIndexedPlayables(id = 1002, is_podcast = false, directory = "yes"))
+
+        indexedQueries.insertGroup(IndexedPodcastEpisodes)
+    }
+
+    @Test
+    fun testObserveAllSortedByTitle() {
+        // ignore accents
+        val expected = listOf(
+            "âspace",
+            "àtitle",
+            "ėspace",
+            "êtitle",
+            "random",
+            "zzz",
+        )
+
+        // when ascending
+        sortQueries.setDetailPodcastCollectionsSort(Sort(CollectionDetailSort.Title, SortDirection.ASCENDING))
+        val actualAsc = queries.selectTracksByIdSorted(collection_id = 2000L).executeAsList()
+        Assert.assertEquals(expected, actualAsc.map { it.title })
+
+        // when descending
+        sortQueries.setDetailPodcastCollectionsSort(Sort(CollectionDetailSort.Title, SortDirection.DESCENDING))
+        val actualDesc = queries.selectTracksByIdSorted(collection_id = 2000L).executeAsList()
+        Assert.assertEquals(expected.reversed(), actualDesc.map { it.title })
+    }
+
+    @Test
+    fun testObserveAllSortedByAuthor() {
+        // when ascending
+        val expectedAsc = listOf(
+            // ignore accents
+            "äaa" to "âspace",
+            "azz" to "àtitle",
+            // second sort on title when same author
+            "zee" to "random",
+            "zee" to "zzz",
+            // <unknown> always last, second sort on title when same author
+            "<unknown>" to "ėspace",
+            "<unknown>" to "êtitle",
+        )
+
+        sortQueries.setDetailPodcastCollectionsSort(Sort(CollectionDetailSort.Author, SortDirection.ASCENDING))
+        val actualAsc = queries.selectTracksByIdSorted(collection_id = 2000L).executeAsList()
+        Assert.assertEquals(expectedAsc, actualAsc.map { it.author to it.title })
+
+        // when descending
+        val expectedDesc = listOf(
+            // second sort on title when same author
+            "zee" to "zzz",
+            "zee" to "random",
+            // ignore accents
+            "azz" to "àtitle",
+            "äaa" to "âspace",
+            // <unknown> always last, second sort on title when same author
+            "<unknown>" to "êtitle",
+            "<unknown>" to "ėspace",
+        )
+
+        sortQueries.setDetailPodcastCollectionsSort(Sort(CollectionDetailSort.Author, SortDirection.DESCENDING))
+        val actualDesc = queries.selectTracksByIdSorted(collection_id = 2000L).executeAsList()
+        Assert.assertEquals(expectedDesc, actualDesc.map { it.author to it.title })
+    }
+
+    @Test
+    fun testObserveAllSortedByAlbumArtist() {
+        // when ascending
+        val expectedAsc = listOf(
+            // ignore accents
+            "äaa2" to "âspace",
+            "azz2" to "àtitle",
+            // second sort on title when same author
+            "zee2" to "random",
+            "zee2" to "zzz",
+            // <unknown> always last, second sort on title when same author
+            "<unknown>" to "ėspace",
+            "<unknown>" to "êtitle",
+        )
+        sortQueries.setDetailPodcastCollectionsSort(Sort(CollectionDetailSort.AlbumArtist, SortDirection.ASCENDING))
+        val actualAsc = queries.selectTracksByIdSorted(collection_id = 2000).executeAsList()
+        Assert.assertEquals(expectedAsc, actualAsc.map { it.album_artist to it.title })
+
+        // when descending
+        val expectedDesc = listOf(
+            // second sort on title when same author
+            "zee2" to "zzz",
+            "zee2" to "random",
+            // ignore accents
+            "azz2" to "àtitle",
+            "äaa2" to "âspace",
+            // <unknown> always last, second sort on title when same author
+            "<unknown>" to "êtitle",
+            "<unknown>" to "ėspace",
+        )
+        sortQueries.setDetailPodcastCollectionsSort(Sort(CollectionDetailSort.AlbumArtist, SortDirection.DESCENDING))
+        val actualDesc = queries.selectTracksByIdSorted(collection_id = 2000).executeAsList()
+        Assert.assertEquals(expectedDesc, actualDesc.map { it.album_artist to it.title })
+    }
+
+    @Test
+    fun testObserveAllSortedByDuration() {
+        // when ascending
+        val expected = listOf(
+            15L to "âspace",
+            20L to "àtitle",
+            // second sort on title when same duration
+            50L to "ėspace",
+            50L to "êtitle",
+            50L to "random",
+            50L to "zzz",
+        )
+        sortQueries.setDetailPodcastCollectionsSort(Sort(CollectionDetailSort.Duration, SortDirection.ASCENDING))
+        val actualAsc = queries.selectTracksByIdSorted(collection_id = 2000L).executeAsList()
+        Assert.assertEquals(expected, actualAsc.map { it.duration to it.title })
+
+        // when descending
+        sortQueries.setDetailPodcastCollectionsSort(Sort(CollectionDetailSort.Duration, SortDirection.DESCENDING))
+        val actualDesc = queries.selectTracksByIdSorted(collection_id = 2000L).executeAsList()
+        Assert.assertEquals(expected.reversed(), actualDesc.map { it.duration to it.title })
+    }
+
+    @Test
+    fun testObserveAllSortedByRecentlyAdded() {
+        // when ascending
+        val expected = listOf(
+            // descending, second sort title ascending
+            40L to "âspace",
+            40L to "ėspace",
+            40L to "random",
+            25L to "zzz",
+            15L to "àtitle",
+            15L to "êtitle",
+        )
+        sortQueries.setDetailPodcastCollectionsSort(Sort(CollectionDetailSort.DateAdded, SortDirection.ASCENDING))
+        val actualAsc = queries.selectTracksByIdSorted(collection_id = 2000L).executeAsList()
+        Assert.assertEquals(expected, actualAsc.map { it.date_added to it.title })
+
+        // when descending
+        sortQueries.setDetailPodcastCollectionsSort(Sort(CollectionDetailSort.DateAdded, SortDirection.DESCENDING))
+        val actualDesc = queries.selectTracksByIdSorted(collection_id = 2000L).executeAsList()
+        Assert.assertEquals(expected.reversed(), actualDesc.map { it.date_added to it.title })
+    }
+
+    @Test
+    fun testObserveAllSortedByTrackNumber() {
+        // when ascending
+        val expected = listOf(
+            Triple(0, 0, "zzz"),
+            Triple(1, 1, "ėspace"),
+            Triple(1, 2, "random"),
+            Triple(2, 1, "âspace"),
+            Triple(2, 2, "àtitle"),
+            Triple(2, 3, "êtitle"),
+        )
+
+        sortQueries.setDetailPodcastCollectionsSort(Sort(CollectionDetailSort.TrackNumber, SortDirection.ASCENDING))
+        val actualAsc = queries.selectTracksByIdSorted(collection_id = 2000L).executeAsList()
+        Assert.assertEquals(expected, actualAsc.map { Triple(it.disc_number, it.track_number, it.title) })
+
+        // when descending
+        sortQueries.setDetailPodcastCollectionsSort(Sort(CollectionDetailSort.TrackNumber, SortDirection.DESCENDING))
+        val actualDesc = queries.selectTracksByIdSorted(collection_id = 2000L).executeAsList()
+        Assert.assertEquals(expected.asReversed(), actualDesc.map { Triple(it.disc_number, it.track_number, it.title) })
+    }
+
+
+    // TODO test flow updates on blacklist and sort
+
+}
---
+++ data/src/test/java/dev/olog/data/collection/PodcastCollectionQueriesTest.kt
@@ -0,0 +1,134 @@
+package dev.olog.data.collection
+
+import dev.olog.data.PodcastCollectionView
+import dev.olog.data.IndexedPlayables
+import dev.olog.data.QueriesConstants
+import dev.olog.data.QueriesConstants.recentlyAddedMaxTime
+import dev.olog.data.TestDatabase
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import kotlin.time.Duration.Companion.days
+
+class PodcastCollectionQueriesTest {
+
+    private val db = TestDatabase()
+    private val indexedQueries = db.indexedPlayablesQueries
+    private val blacklistQueries = db.blacklistQueries
+    private val queries = db.podcastCollectionQueries
+
+    @Before
+    fun setup() {
+        blacklistQueries.insert("dir")
+
+        indexedQueries.insert(IndexedPlayables(id = 100, collection_id = 10, is_podcast = true))
+        indexedQueries.insert(IndexedPlayables(id = 101, collection_id = 10, is_podcast = true))
+        indexedQueries.insert(IndexedPlayables(id = 102, collection_id = 10, is_podcast = true))
+        indexedQueries.insert(IndexedPlayables(id = 103, collection_id = 20, is_podcast = true))
+        indexedQueries.insert(IndexedPlayables(id = 104, collection_id = 20, is_podcast = true))
+        // below should be filtered
+        indexedQueries.insert(IndexedPlayables(id = 105, collection_id = 30, is_podcast = false))
+        indexedQueries.insert(IndexedPlayables(id = 106, collection_id = 30, is_podcast = true, directory = "dir"))
+    }
+
+    @Test
+    fun `test selectAll`() {
+        val actual = queries.selectAll().executeAsList()
+        val expected = listOf(
+            PodcastCollectionView(id = 10, songs = 3),
+            PodcastCollectionView(id = 20, songs = 2),
+        )
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `test selectById`() {
+        val actual = queries.selectById(10).executeAsOne()
+        val expected = PodcastCollectionView(id = 10, songs = 3)
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `test selectById, should be null when not present`() {
+        val actual = queries.selectById(50).executeAsOneOrNull()
+        Assert.assertEquals(null, actual)
+    }
+
+    @Test
+    fun `test selectRecentlyAdded`() {
+        var id = 0L
+        fun song(authorId: Long, dateAdded: Long, directory: String = "") = IndexedPlayables(
+            id = ++id,
+            collection_id = authorId,
+            date_added = dateAdded,
+            is_podcast = true,
+            directory = directory
+        )
+
+        indexedQueries.deleteAll()
+
+        val unixTimestamp = QueriesConstants.unitTimestamp()
+        indexedQueries.insert(song(authorId = 10, dateAdded = unixTimestamp))
+        indexedQueries.insert(song(authorId = 20, dateAdded = unixTimestamp - recentlyAddedMaxTime + 1.days.inWholeSeconds))
+        indexedQueries.insert(song(authorId = 30, dateAdded = unixTimestamp - recentlyAddedMaxTime + 3.days.inWholeSeconds))
+        indexedQueries.insert(song(authorId = 40, dateAdded = unixTimestamp - recentlyAddedMaxTime + 2.days.inWholeSeconds))
+        // below should be skipped
+        indexedQueries.insert(song(authorId = 50, dateAdded = unixTimestamp - recentlyAddedMaxTime - 1.days.inWholeSeconds))
+        indexedQueries.insert(song(authorId = 60, dateAdded = unixTimestamp, directory = "dir"))
+
+        val actual = queries.selectRecentlyAdded().executeAsList()
+        val expected = listOf(
+            PodcastCollectionView(id = 10, songs = 1, dateAdded = unixTimestamp),
+            PodcastCollectionView(id = 30, songs = 1, dateAdded = unixTimestamp - recentlyAddedMaxTime + 3.days.inWholeSeconds),
+            PodcastCollectionView(id = 40, songs = 1, dateAdded = unixTimestamp - recentlyAddedMaxTime + 2.days.inWholeSeconds),
+            PodcastCollectionView(id = 20, songs = 1, dateAdded = unixTimestamp - recentlyAddedMaxTime + 1.days.inWholeSeconds),
+        )
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `test lastPlayed, max 10 items`() {
+        indexedQueries.insert(IndexedPlayables(id = 1, collection_id = 1, is_podcast = true))
+        indexedQueries.insert(IndexedPlayables(id = 2, collection_id = 2, is_podcast = true))
+        indexedQueries.insert(IndexedPlayables(id = 3, collection_id = 3, is_podcast = true))
+        indexedQueries.insert(IndexedPlayables(id = 4, collection_id = 4, is_podcast = true))
+        indexedQueries.insert(IndexedPlayables(id = 5, collection_id = 5, is_podcast = true))
+        indexedQueries.insert(IndexedPlayables(id = 6, collection_id = 6, is_podcast = true))
+        indexedQueries.insert(IndexedPlayables(id = 7, collection_id = 7, is_podcast = true))
+        indexedQueries.insert(IndexedPlayables(id = 8, collection_id = 8, is_podcast = true))
+        indexedQueries.insert(IndexedPlayables(id = 9, collection_id = 9, is_podcast = true))
+        indexedQueries.insert(IndexedPlayables(id = 10, collection_id = 10, is_podcast = true))
+        indexedQueries.insert(IndexedPlayables(id = 11, collection_id = 11, is_podcast = true))
+        indexedQueries.insert(IndexedPlayables(id = 12, collection_id = 12, is_podcast = true))
+        indexedQueries.insert(IndexedPlayables(id = 13, collection_id = 12, is_podcast = true))
+
+        queries.insertRecentlyPlayed(id = 1, date_played = 10)
+        queries.insertRecentlyPlayed(id = 2, date_played = 20)
+        queries.insertRecentlyPlayed(id = 3, date_played = 30)
+        queries.insertRecentlyPlayed(id = 4, date_played = 40)
+        queries.insertRecentlyPlayed(id = 5, date_played = 50)
+        queries.insertRecentlyPlayed(id = 6, date_played = 60)
+        queries.insertRecentlyPlayed(id = 7, date_played = 70)
+        queries.insertRecentlyPlayed(id = 8, date_played = 100)
+        queries.insertRecentlyPlayed(id = 9, date_played = 80)
+        queries.insertRecentlyPlayed(id = 10, date_played = 5)
+        queries.insertRecentlyPlayed(id = 11, date_played = 2)
+        queries.insertRecentlyPlayed(id = 12, date_played = 90)
+
+        val actual = queries.selectRecentlyPlayed().executeAsList()
+        val expected = listOf(
+            PodcastCollectionView(id = 8, songs = 1),
+            PodcastCollectionView(id = 12, songs = 2),
+            PodcastCollectionView(id = 9, songs = 1),
+            PodcastCollectionView(id = 7, songs = 1),
+            PodcastCollectionView(id = 6, songs = 1),
+            PodcastCollectionView(id = 5, songs = 1),
+            PodcastCollectionView(id = 4, songs = 1),
+            PodcastCollectionView(id = 3, songs = 1),
+            PodcastCollectionView(id = 2, songs = 1),
+            PodcastCollectionView(id = 1, songs = 1),
+        )
+        Assert.assertEquals(expected, actual)
+    }
+
+}
