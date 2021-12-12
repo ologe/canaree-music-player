cca9c0ef0de050e7619d6fb39fcfe9f96f56fc5c
Eugeniu Olog <eugeniu.olog@gmail.com>
2021-12-12 12:34:16 +0100

    - created PodcastPosition + tests

---
+++ data/src/test/java/dev/olog/data/PodcastPositionQueriesTest.kt
@@ -0,0 +1,31 @@
+package dev.olog.data
+
+import org.junit.Assert
+import org.junit.Test
+
+class PodcastPositionQueriesTest {
+
+    private val db = TestDatabase()
+    private val queries = db.podcastPositionQueries
+
+    @Test
+    fun test() {
+        queries.insert(1, 1000)
+
+        Assert.assertEquals(
+            Podcast_position(1, 1000),
+            queries.selectById(1).executeAsOne(),
+        )
+    }
+
+    @Test
+    fun `test, should be null`() {
+        queries.insert(1, 1000)
+
+        Assert.assertEquals(
+            null,
+            queries.selectById(2).executeAsOneOrNull(),
+        )
+    }
+
+}
