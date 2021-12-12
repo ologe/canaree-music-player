a4141fbc0b4ba0167ce5c5cc9f7b633cb026630d
Eugeniu Olog <eugeniu.olog@gmail.com>
2021-12-12 12:24:35 +0100

    - added sync to lyrics

--- data/src/test/java/dev/olog/data/LyricsQueriesTest.kt
+++ data/src/test/java/dev/olog/data/LyricsQueriesTest.kt
@@ -10,20 +10,112 @@ class LyricsQueriesTest {
 
     @Test
     fun `test select`() {
-        queries.insert(1, "text")
+        queries.insertText(1, "text")
 
         val actual = queries.selectById(1).executeAsOne()
-        val expected = Lyrics(1, "text")
+        val expected = Lyrics(1, "text", 0)
 
         Assert.assertEquals(expected, actual)
     }
 
     @Test
     fun `test select missing item`() {
-        queries.insert(1, "text")
+        queries.insertText(1, "text")
 
         val actual = queries.selectById(2).executeAsOneOrNull()
         Assert.assertEquals(null, actual)
+    }
+
+    @Test
+    fun `test replace, start with text`() {
+        // insert text
+        queries.insertText(1, "text")
+
+        Assert.assertEquals(
+            Lyrics(1, "text", 0),
+            queries.selectById(1).executeAsOne()
+        )
+
+        // replace text
+        queries.insertText(1, "abc")
+
+        Assert.assertEquals(
+            Lyrics(1, "abc", 0),
+            queries.selectById(1).executeAsOne()
+        )
+
+        // insert sync
+        queries.insertSync(1, 1000)
+
+        Assert.assertEquals(
+            Lyrics(1, "abc", 1000),
+            queries.selectById(1).executeAsOne()
+        )
+
+        // replace sync
+        queries.insertSync(1, 500)
+
+        Assert.assertEquals(
+            Lyrics(1, "abc", 500),
+            queries.selectById(1).executeAsOne()
+        )
+    }
+
+    @Test
+    fun `test replace, start with sync`() {
+        // insert sync
+        queries.insertSync(1, 1000)
+
+        Assert.assertEquals(
+            Lyrics(1, "", 1000),
+            queries.selectById(1).executeAsOne()
+        )
+
+        // replace sync
+        queries.insertSync(1, 500)
+
+        Assert.assertEquals(
+            Lyrics(1, "", 500),
+            queries.selectById(1).executeAsOne()
+        )
+
+        // insert first item text
+        queries.insertText(1, "text")
+
+        Assert.assertEquals(
+            Lyrics(1, "text", 500),
+            queries.selectById(1).executeAsOne()
+        )
+
+        // replace text
+        queries.insertText(1, "abc")
+
+        Assert.assertEquals(
+            Lyrics(1, "abc", 500),
+            queries.selectById(1).executeAsOne()
+        )
+    }
+
+    @Test
+    fun `test initDefault, success when not already present`() {
+        queries.initDefault(1)
+
+        Assert.assertEquals(
+            Lyrics(1, "", 0),
+            queries.selectById(1).executeAsOne()
+        )
+    }
+
+    @Test
+    fun `test initDefault, fail when already present`() {
+        queries.insertText(1, "abc")
+
+        queries.initDefault(1)
+
+        Assert.assertEquals(
+            Lyrics(1, "abc", 0),
+            queries.selectById(1).executeAsOne()
+        )
     }
 
 }
