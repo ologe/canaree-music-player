c3ea38ea54f90bfac477a07a7ffd594019fa0894
Eugeniu Olog <eugeniu.olog@gmail.com>
2021-12-30 19:29:55 +0100

    added insert method

--- data/src/main/java/dev/olog/data/history/HistoryRepository.kt
+++ data/src/main/java/dev/olog/data/history/HistoryRepository.kt
@@ -1,7 +1,9 @@
 package dev.olog.data.history
 
-import dev.olog.core.entity.track.Song
-import dev.olog.core.gateway.HistoryGateway
+import dev.olog.core.DateTimeFactory
+import dev.olog.core.MediaUri
+import dev.olog.core.history.HistoryGateway
+import dev.olog.core.track.Song
 import dev.olog.core.schedulers.Schedulers
 import dev.olog.data.HistoryQueries
 import dev.olog.data.extension.mapToFlowList
@@ -10,11 +12,13 @@ import dev.olog.data.playable.Songs_view
 import dev.olog.data.playable.toDomain
 import dev.olog.shared.mapListItem
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 internal class HistoryRepository @Inject constructor(
     private val schedulers: Schedulers,
     private val queries: HistoryQueries,
+    private val dateTimeFactory: DateTimeFactory,
 ) : HistoryGateway {
 
     override fun getSongs(): List<Song> {
@@ -39,5 +43,9 @@ internal class HistoryRepository @Inject constructor(
         return queries.selectAllPodcastEpisodes()
             .mapToFlowList(schedulers.io)
             .mapListItem(Podcast_episodes_view::toDomain)
+    }
+
+    override suspend fun insert(uri: MediaUri) = withContext(schedulers.io) {
+        queries.insert(uri.id, dateTimeFactory.currentTimeMillis())
     }
 }
