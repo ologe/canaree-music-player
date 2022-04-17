bdc0726e08ed2fa5116ffc9c85ddfb1b412b651b
Eugeniu Olog <eugeniu.olog@gmail.com>
2022-04-17 22:29:12 +0200

    replace custom scope with runBlocking

--- image-provider/src/main/java/dev/olog/image/provider/fetcher/GlideOriginalImageFetcher.kt
+++ image-provider/src/main/java/dev/olog/image/provider/fetcher/GlideOriginalImageFetcher.kt
@@ -8,14 +8,9 @@ import dev.olog.core.MediaId
 import dev.olog.core.entity.track.Song
 import dev.olog.core.gateway.podcast.PodcastGateway
 import dev.olog.core.gateway.track.SongGateway
-import dev.olog.image.provider.executor.GlideScope
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.yield
 import java.io.InputStream
-import java.lang.RuntimeException
-import java.util.concurrent.CancellationException
 
 class GlideOriginalImageFetcher(
     private val context: Context,
@@ -23,41 +18,42 @@ class GlideOriginalImageFetcher(
     private val songGateway: SongGateway,
     private val podcastGateway: PodcastGateway
 
-) : DataFetcher<InputStream>, CoroutineScope by GlideScope() {
+) : DataFetcher<InputStream> {
 
     override fun getDataClass(): Class<InputStream> = InputStream::class.java
     override fun getDataSource(): DataSource = DataSource.LOCAL
 
-    override fun loadData(priority: Priority, callback: DataFetcher.DataCallback<in InputStream>) {
-        launch {
-            val id = getId()
-            if (id == -1L) {
-                callback.onLoadFailed(Exception("item not found for id$id"))
-                return@launch
-            }
+    override fun loadData(
+        priority: Priority,
+        callback: DataFetcher.DataCallback<in InputStream>
+    ) = runBlocking {
+        val id = getId()
+        if (id == -1L) {
+            callback.onLoadFailed(Exception("item not found for id$id"))
+            return@runBlocking
+        }
 
-            val song: Song? = when {
-                mediaId.isAlbum -> songGateway.getByAlbumId(id)
-                mediaId.isPodcastAlbum -> podcastGateway.getByAlbumId(id)
-                mediaId.isLeaf && !mediaId.isPodcast -> songGateway.getByParam(id)
-                mediaId.isLeaf && mediaId.isPodcast -> podcastGateway.getByParam(id)
-                else -> {
-                    callback.onLoadFailed(IllegalArgumentException("not a valid media id=$mediaId"))
-                    return@launch
-                }
+        val song: Song? = when {
+            mediaId.isAlbum -> songGateway.getByAlbumId(id)
+            mediaId.isPodcastAlbum -> podcastGateway.getByAlbumId(id)
+            mediaId.isLeaf && !mediaId.isPodcast -> songGateway.getByParam(id)
+            mediaId.isLeaf && mediaId.isPodcast -> podcastGateway.getByParam(id)
+            else -> {
+                callback.onLoadFailed(IllegalArgumentException("not a valid media id=$mediaId"))
+                return@runBlocking
             }
-            yield()
+        }
+        yield()
 
-            if (song == null) {
-                callback.onLoadFailed(IllegalArgumentException("track not found for id $id"))
-                return@launch
-            }
-            try {
-                val stream = OriginalImageFetcher.loadImage(context, song)
-                callback.onDataReady(stream)
-            } catch (ex: Throwable) {
-                callback.onLoadFailed(RuntimeException(ex))
-            }
+        if (song == null) {
+            callback.onLoadFailed(IllegalArgumentException("track not found for id $id"))
+            return@runBlocking
+        }
+        try {
+            val stream = OriginalImageFetcher.loadImage(context, song)
+            callback.onDataReady(stream)
+        } catch (ex: Throwable) {
+            callback.onLoadFailed(RuntimeException(ex))
         }
     }
 
@@ -78,7 +74,6 @@ class GlideOriginalImageFetcher(
     }
 
     override fun cancel() {
-        cancel(CancellationException())
-    }
 
+    }
 }
