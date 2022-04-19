8b57f6c10727dff175822c96d271c8efa9c94cf0
Eugeniu Olog <eugeniu.olog@gmail.com>
2022-04-19 19:01:57 +0200

    fix crash on view non attached

--- presentation/src/main/java/dev/olog/presentation/widgets/textview/ExplicitView.kt
+++ presentation/src/main/java/dev/olog/presentation/widgets/textview/ExplicitView.kt
@@ -5,30 +5,35 @@ import android.content.res.ColorStateList
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.view.isVisible
+import dev.olog.shared.android.extensions.coroutineScope
 import dev.olog.shared.android.extensions.textColorPrimary
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
 
 class ExplicitView(
     context: Context,
     attrs: AttributeSet
-) : AppCompatImageView(context, attrs), CoroutineScope by MainScope() {
+) : AppCompatImageView(context, attrs) {
 
-    private var job: Job? = null
+    private val publisher = MutableStateFlow("")
 
     init {
         imageTintList = ColorStateList.valueOf(context.textColorPrimary())
     }
 
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+
+        publisher
+            .map { title -> title.contains("explicit", ignoreCase = true) }
+            .flowOn(Dispatchers.Default)
+            .onEach { show -> isVisible = show }
+            .launchIn(coroutineScope)
+    }
+
     fun onItemChanged(title: String) {
         isVisible = false
-
-        job?.cancel()
-        job = launch(Dispatchers.Default) {
-            val show = title.contains("explicit", ignoreCase = true)
-            withContext(Dispatchers.Main) {
-                isVisible = show
-            }
-        }
+        publisher.value = title
     }
 
 }
