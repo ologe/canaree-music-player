44c5f0e9fff45f3e9b044e92c181939ab0467ed2
Eugeniu Olog <eugeniu.olog@gmail.com>
2022-12-30 15:30:45 +0100

    coroutines scope fixes

--- service-music/src/main/java/dev/olog/service/music/state/MusicServiceMetadata.kt
+++ service-music/src/main/java/dev/olog/service/music/state/MusicServiceMetadata.kt
@@ -5,39 +5,38 @@ import android.content.Context
 import android.content.Intent
 import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaSessionCompat
-import android.util.Log
-import androidx.lifecycle.DefaultLifecycleObserver
-import androidx.lifecycle.LifecycleOwner
-import com.bumptech.glide.request.target.Target
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.coroutineScope
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
 import dev.olog.core.prefs.MusicPreferencesGateway
 import dev.olog.image.provider.GlideUtils
 import dev.olog.image.provider.getCachedBitmap
-import dagger.hilt.android.scopes.ServiceScoped
+import dev.olog.injection.dagger.ServiceLifecycle
+import dev.olog.intents.Classes
+import dev.olog.intents.MusicConstants
+import dev.olog.intents.WidgetConstants
 import dev.olog.service.music.interfaces.IPlayerLifecycle
 import dev.olog.service.music.model.MediaEntity
 import dev.olog.service.music.model.MetadataEntity
 import dev.olog.service.music.model.SkipType
-import dev.olog.intents.Classes
-import dev.olog.intents.MusicConstants
-import dev.olog.intents.WidgetConstants
 import dev.olog.service.music.utils.putBoolean
-import dev.olog.shared.CustomScope
 import dev.olog.shared.android.extensions.getAppWidgetsIdsFor
-import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.collect
+import dev.olog.shared.autoDisposeJob
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @ServiceScoped
 internal class MusicServiceMetadata @Inject constructor(
+    @ServiceLifecycle private val lifecycle: Lifecycle,
     @ApplicationContext private val context: Context,
     private val mediaSession: MediaSessionCompat,
     playerLifecycle: IPlayerLifecycle,
     private val musicPrefs: MusicPreferencesGateway
 
-) : IPlayerLifecycle.Listener,
-    DefaultLifecycleObserver,
-    CoroutineScope by CustomScope() {
+) : IPlayerLifecycle.Listener {
 
     companion object {
         @JvmStatic
@@ -47,14 +46,14 @@ internal class MusicServiceMetadata @Inject constructor(
     private val builder = MediaMetadataCompat.Builder()
 
     private var showLockScreenArtwork = false
+    private var job by autoDisposeJob()
 
     init {
         playerLifecycle.addListener(this)
 
-        launch {
-            musicPrefs.observeShowLockscreenArtwork()
-                .collect { showLockScreenArtwork = it }
-        }
+        musicPrefs.observeShowLockscreenArtwork()
+            .onEach { showLockScreenArtwork = it }
+            .launchIn(lifecycle.coroutineScope)
     }
 
     override fun onPrepare(metadata: MetadataEntity) {
@@ -66,44 +65,35 @@ internal class MusicServiceMetadata @Inject constructor(
         notifyWidgets(metadata.entity)
     }
 
-    override fun onDestroy(owner: LifecycleOwner) {
-        cancel()
-    }
-
     private fun update(metadata: MetadataEntity) {
-        Log.v(TAG, "update metadata ${metadata.entity.title}, skip type=${metadata.skipType}")
+        job = null
 
-        launch {
+        val entity = metadata.entity
 
-            val entity = metadata.entity
+        builder.putString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID, entity.mediaId.toString())
+            .putString(MediaMetadataCompat.METADATA_KEY_TITLE, entity.title)
+            .putString(MediaMetadataCompat.METADATA_KEY_ARTIST, entity.artist)
+            .putString(MediaMetadataCompat.METADATA_KEY_ALBUM, entity.album)
+            .putString(MediaMetadataCompat.METADATA_KEY_DISPLAY_TITLE, entity.title)
+            .putString(MediaMetadataCompat.METADATA_KEY_DISPLAY_SUBTITLE, entity.artist)
+            .putString(MediaMetadataCompat.METADATA_KEY_DISPLAY_DESCRIPTION, entity.album)
+            .putLong(MediaMetadataCompat.METADATA_KEY_DURATION, entity.duration)
+            .putString(MusicConstants.PATH, entity.path)
+            .putBoolean(MusicConstants.IS_PODCAST, entity.isPodcast)
+            .putBoolean(MusicConstants.SKIP_NEXT, metadata.skipType == SkipType.SKIP_NEXT)
+            .putBoolean(MusicConstants.SKIP_PREVIOUS, metadata.skipType == SkipType.SKIP_PREVIOUS)
 
-            builder.putString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID, entity.mediaId.toString())
-                .putString(MediaMetadataCompat.METADATA_KEY_TITLE, entity.title)
-                .putString(MediaMetadataCompat.METADATA_KEY_ARTIST, entity.artist)
-                .putString(MediaMetadataCompat.METADATA_KEY_ALBUM, entity.album)
-                .putString(MediaMetadataCompat.METADATA_KEY_DISPLAY_TITLE, entity.title)
-                .putString(MediaMetadataCompat.METADATA_KEY_DISPLAY_SUBTITLE, entity.artist)
-                .putString(MediaMetadataCompat.METADATA_KEY_DISPLAY_DESCRIPTION, entity.album)
-                .putLong(MediaMetadataCompat.METADATA_KEY_DURATION, entity.duration)
-                .putString(MusicConstants.PATH, entity.path)
-                .putBoolean(MusicConstants.IS_PODCAST, entity.isPodcast)
-                .putBoolean(MusicConstants.SKIP_NEXT, metadata.skipType == SkipType.SKIP_NEXT)
-                .putBoolean(MusicConstants.SKIP_PREVIOUS, metadata.skipType == SkipType.SKIP_PREVIOUS)
+        mediaSession.setMetadata(builder.build())
 
-            yield()
-
+        job = lifecycle.coroutineScope.launch {
             if (showLockScreenArtwork) {
                 val bitmap = context.getCachedBitmap(entity.mediaId, GlideUtils.OVERRIDE_BIG)
-                yield()
                 builder.putBitmap(MediaMetadataCompat.METADATA_KEY_ALBUM_ART, bitmap)
             }
-            mediaSession.setMetadata(builder.build())
         }
     }
 
     private fun notifyWidgets(entity: MediaEntity) {
-        Log.v(TAG, "notify widgets ${entity.title}")
-
         for (clazz in Classes.widgets) {
             val ids = context.getAppWidgetsIdsFor(clazz)
 
