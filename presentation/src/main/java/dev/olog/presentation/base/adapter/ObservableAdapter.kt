2163c97afef847949d413d1e24a0c232284d0bb7
Eugeniu Olog <eugeniu.olog@gmail.com>
2024-07-14 18:47:52 +0200

    remove custom scope

--- presentation/src/main/java/dev/olog/presentation/base/adapter/ObservableAdapter.kt
+++ presentation/src/main/java/dev/olog/presentation/base/adapter/ObservableAdapter.kt
@@ -9,7 +9,6 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import dev.olog.presentation.model.BaseModel
-import dev.olog.shared.CustomScope
 import dev.olog.shared.android.utils.assertBackgroundThread
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.ConflatedBroadcastChannel
@@ -21,7 +20,7 @@ abstract class ObservableAdapter<T : BaseModel>(
 
 ) : RecyclerView.Adapter<DataBoundViewHolder>(),
     DefaultLifecycleObserver,
-    CoroutineScope by CustomScope() {
+    CoroutineScope by MainScope() {
 
     protected val dataSet = mutableListOf<T>()
     private var neverEmitted = true
@@ -45,7 +44,7 @@ abstract class ObservableAdapter<T : BaseModel>(
     init {
         lifecycle.addObserver(this)
 
-        launch {
+        launch(Dispatchers.Default) {
             channel.asFlow()
                 .distinctUntilChanged()
                 .collect { list ->
--- service-music/src/main/java/dev/olog/service/music/queue/QueueImpl.kt
+++ service-music/src/main/java/dev/olog/service/music/queue/QueueImpl.kt
@@ -17,7 +17,6 @@ import dev.olog.service.music.model.MediaEntity
 import dev.olog.service.music.model.PositionInQueue
 import dev.olog.service.music.model.toMediaEntity
 import dev.olog.service.music.state.MusicServiceRepeatMode
-import dev.olog.shared.CustomScope
 import dev.olog.shared.android.utils.assertBackgroundThread
 import dev.olog.shared.android.utils.assertMainThread
 import dev.olog.shared.clamp
