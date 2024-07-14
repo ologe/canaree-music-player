474db460488c82969fc0198dbe76169732991cff
Eugeniu Olog <eugeniu.olog@gmail.com>
2024-07-14 18:38:45 +0200

    remove custom scope

--- service-music/src/main/java/dev/olog/service/music/queue/MediaSessionQueue.kt
+++ service-music/src/main/java/dev/olog/service/music/queue/MediaSessionQueue.kt
@@ -3,12 +3,10 @@ package dev.olog.service.music.queue
 import android.support.v4.media.MediaDescriptionCompat
 import android.support.v4.media.session.MediaSessionCompat
 import android.util.Log
-import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.coroutineScope
 import dev.olog.core.ServiceLifecycle
 import dev.olog.service.music.model.MediaEntity
-import dev.olog.shared.CustomScope
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.ConflatedBroadcastChannel
 import kotlinx.coroutines.flow.asFlow
@@ -19,8 +17,7 @@ import javax.inject.Inject
 internal class MediaSessionQueue @Inject constructor(
     @ServiceLifecycle lifecycle: Lifecycle,
     private val mediaSession: MediaSessionCompat
-) : DefaultLifecycleObserver,
-    CoroutineScope by CustomScope() {
+) {
 
     companion object {
         @JvmStatic
@@ -32,15 +29,13 @@ internal class MediaSessionQueue @Inject constructor(
     private val immediateChannel = ConflatedBroadcastChannel<List<MediaEntity>>()
 
     init {
-        lifecycle.addObserver(this)
-
-        launch {
+        lifecycle.coroutineScope.launch {
             delayedChannel.asFlow()
                 .debounce(DELAY)
                 .collect { publish(it) }
         }
 
-        launch {
+        lifecycle.coroutineScope.launch {
             immediateChannel.asFlow()
                 .collect { publish(it) }
         }
@@ -63,10 +58,6 @@ internal class MediaSessionQueue @Inject constructor(
     fun onNextImmediate(list: List<MediaEntity>) {
         Log.v(TAG, "on next immediate")
         immediateChannel.offer(list)
-    }
-
-    override fun onDestroy(owner: LifecycleOwner) {
-        cancel()
     }
 
     private fun MediaEntity.toQueueItem(): MediaSessionCompat.QueueItem {
