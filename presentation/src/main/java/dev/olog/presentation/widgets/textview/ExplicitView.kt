3e628b2013616435dd142e011eab956c84647380
Eugeniu Olog <eugeniu.olog@gmail.com>
2024-07-14 19:23:02 +0200

    remove custom coroutine scope

--- presentation/src/main/java/dev/olog/presentation/widgets/textview/ExplicitView.kt
+++ presentation/src/main/java/dev/olog/presentation/widgets/textview/ExplicitView.kt
@@ -6,12 +6,13 @@ import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
 import dev.olog.shared.android.extensions.textColorPrimary
 import dev.olog.shared.android.extensions.toggleVisibility
+import dev.olog.shared.android.viewScope
 import kotlinx.coroutines.*
 
 class ExplicitView(
     context: Context,
     attrs: AttributeSet
-) : AppCompatImageView(context, attrs), CoroutineScope by MainScope() {
+) : AppCompatImageView(context, attrs) {
 
     private var job: Job? = null
 
@@ -23,7 +24,7 @@ class ExplicitView(
         toggleVisibility(visible = false, gone = true)
 
         job?.cancel()
-        job = launch(Dispatchers.Default) {
+        job = viewScope.launch(Dispatchers.Default) {
             val show = title.contains("explicit", ignoreCase = true)
             withContext(Dispatchers.Main) {
                 toggleVisibility(visible = show, gone = true)
