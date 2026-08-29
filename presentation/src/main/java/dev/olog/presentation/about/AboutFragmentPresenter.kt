4cbbaf71f9f8b9a1bc3d2ac72cc3f8a627cf11e2
Eugeniu Olog <eugeniu.olog@gmail.com>
2026-08-29 12:17:19 +0200

    bump agp to 7.0.4

--- presentation/src/main/java/dev/olog/presentation/about/AboutFragmentPresenter.kt
+++ presentation/src/main/java/dev/olog/presentation/about/AboutFragmentPresenter.kt
@@ -57,7 +57,7 @@ class AboutFragmentPresenter(
             type = R.layout.item_about,
             mediaId = MediaId.headerId("version id"),
             title = context.getString(R.string.about_version),
-            subtitle = BuildConfig.VERSION_NAME
+            subtitle = "BuildConfig.VERSION_NAME" // todo
         ),
 
         DisplayableHeader(
