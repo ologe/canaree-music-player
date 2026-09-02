05a9ae42f7f21db93ba15838f91d63c76ed42796
Eugeniu Olog <eugeniu.olog@gmail.com>
2026-09-02 20:12:24 +0200

    androix dependencies update

--- service-music/src/main/java/dev/olog/service/music/BaseMusicService.kt
+++ service-music/src/main/java/dev/olog/service/music/BaseMusicService.kt
@@ -142,7 +142,8 @@ abstract class BaseMusicService : MediaBrowserServiceCompat(),
         }
     }
 
-    override fun getLifecycle(): Lifecycle = dispatcher.lifecycle
+    override val lifecycle: Lifecycle
+        get() = dispatcher.lifecycle
 
     protected abstract fun handleMediaButton(intent: Intent)
 
